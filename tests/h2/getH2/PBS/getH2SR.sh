3db392c425fce24998f85805688ef400a265ff46
nobrien97 <nicholasobrien97@gmail.com>
2022-11-02 17:04:45 +1000

    New job to calculate h2

--- tests/h2/getH2/PBS/getH2SR.sh
+++ tests/h2/getH2/PBS/getH2SR.sh
@@ -6,11 +6,18 @@ cd $PBS_JOBFS
 SECONDS=0
 
 GDATAPATH=/g/data/ht96/nb9894/h2
+SCRATCHPATH=/scratch/ht96/nb9894/h2/getH2
 
-# Only one argument in this case: the run number (starts at 0) 
+# Two arguments: 
+# run number (starts at 0)
+# chunk (starts at 1): 
+#        since we have heaps of files, we want to make sure that we don't write too many for /scratch/
+#        so we chunk our data into sets so we can progressively combine files and delete them without
+#        exceeding the limit.
 RUN=$1
+CHUNK=$2
 
-if [ -f $HOME/tests/h2/getH2/done/${RUN} ]; then
+if [ -f $HOME/tests/h2/getH2/done/${RUN}_${CHUNK} ]; then
     echo "$RUN already done! Moving to next simulation."
     exit 0
 fi
@@ -21,22 +28,32 @@ fi
 ## First get the min/max rows to read: pedigree is offset by 1 because header, haplotype doesn't have one
 MIN_HAPLO=$((1+$RUN*1000))
 MAX_HAPLO=$(((1+$RUN)*1000))
-MIN_PED=$(((1+$RUN*500)+1))
-MAX_PED=$((((1+$RUN)*500)+1))
+# MIN_PED=$(((1+$RUN*500)+1))
+# MAX_PED=$((((1+$RUN)*500)+1))
 
 # Save files
 tail -n "+$MIN_HAPLO" $GDATAPATH/slim_haplo.csv | head -n "$(($MAX_HAPLO-$MIN_HAPLO+1))" > slim_haplo_sbst_$RUN.csv
-tail -n "+$MIN_PED" $GDATAPATH/slim_pedigree.csv | head -n "$(($MAX_PED-$MIN_PED+1))" > slim_ped_sbst_$RUN.csv
+# tail -n "+$MIN_PED" $GDATAPATH/slim_pedigree.csv | head -n "$(($MAX_PED-$MIN_PED+1))" > slim_ped_sbst_$RUN.csv
+tail -n "+$(($RUN+1))" $GDATAPATH/slim_sampled_pheno.csv | head -n 1 > slim_pheno_sbst_$RUN.csv
 
-RSCRIPTNAME=$HOME/tests/h2/getH2/R/h2_singleshot.R
+RSCRIPTNAME=$HOME/tests/h2/getH2/R/calcH2.R
 
 echo "Running RUNindex = $RUN...\n"
-Rscript ${RSCRIPTNAME} ${RUN}
+Rscript ${RSCRIPTNAME} ${RUN} ${CHUNK}
+
+
+# Create file to show what we've already done if we get interrupted
+touch $HOME/tests/h2/getH2/done/${RUN}_${CHUNK}
+
+# Check if we're the last in a chunk, if we are we need to do some cleanup, otherwise we can continue
+# Chunks should consist of 2016 files
+if [ $(ls $HOME/tests/h2/getH2/done/*_${CHUNK} | wc -l) == 2016]; then
+    echo "Chunk $CHUNK done, combining chunk files and cleaning up..."
+    cat $SCRATCHPATH/*_${CHUNK}.csv >> $SCRATCHPATH/out_h2_${CHUNK}_done.csv
+    rm $SCRATCHPATH/*_${CHUNK}.csv
+fi
 
 
 DURATION=$SECONDS
 echo "Run RUNindex = $RUN, finished!"
 echo "$(($DURATION / 3600)) hours, $((($DURATION / 60) % 60)) minutes, and $(($DURATION % 60)) seconds elapsed."
-
-# Create file to show what we've already done if we get interrupted
-touch $HOME/tests/h2/getH2/done/${RUN}
---
+++ tests/h2/getH2/R/calcH2.R
@@ -0,0 +1,121 @@
+# R Script to calculate heritability given some data
+library(sommer)
+
+# Get command line arguments
+args <- commandArgs(trailingOnly = T)
+run <- as.numeric(args[1])
+chunk <- as.numeric(args[2])
+run_chunk <- paste(run, chunk, sep = "_")
+
+# Load function for extracting genotypes
+source("~/tests/h2/R/helpFns.R")
+
+# Read data and add names
+haplos <- read.csv(paste0("slim_haplo_sbst_", run, ".csv"), header = F)[, -(1:3)]
+names(haplos) <- paste0("q_", seq_len(ncol(haplos)))
+phenos <- unlist(read.csv(paste0("slim_pheno_sbst_", run, ".csv"), header = F))
+names(phenos) <- NULL
+# From the phenos file, extract gen, seed, modelindex and remove them
+## Use these later to identify output
+run_info <- phenos[1:3]
+phenos <- phenos[-(1:3)]
+
+# Check to make sure there is variability
+if (!max(haplos)) {
+   result <- data.frame(gen = run_info[1],
+                            seed = as.character(run_info[2]),
+                            modelindex = run_info[3],
+                            VarA = 0, 
+                            VarD = 0,
+                            VarAA = 0,
+                            VarR = 0,
+                            VarA.SE = 0,
+                            VarD.SE = 0,
+                            VarAA.SE = 0,
+                            VarR.SE = 0,
+                            H2.A = 0,
+                            H2.D = 0,
+                            H2.AA = 0,
+                            AIC = 0
+                            )
+    write.table(result, paste0("/scratch/ht96/nb9894/h2/getH2/out_h2_", run_chunk, ".csv"), sep = ",", row.names = F, col.names = F)    q(save = "no")
+    q(save = "no")
+}
+
+# Get genotypes from haplos
+genos <- hapToGen(haplos)
+geno_names <- paste0("i_", seq_len(nrow(genos)))
+pheno_dat <- data.frame(y = phenos,
+                        id = as.factor(geno_names))
+
+# Calculate genomic relationship matrices
+A <- A.mat(as.matrix(genos)-1)
+D <- D.mat(as.matrix(genos)-1)
+AA <- A*A
+
+# Add names to dims
+dimnames(A) <- list(geno_names, geno_names)
+dimnames(D) <- list(geno_names, geno_names)
+dimnames(AA) <- list(geno_names, geno_names)
+
+# Add extra IDs for D and AA
+pheno_dat$idd <- pheno_dat$id
+pheno_dat$idaa <- pheno_dat$id
+
+######################################################################
+# This code is adapted from Onigo et al. 2021, thanks to Akio Onogi
+# for sharing code samples
+
+# Fit models using A, D, and AA GRMs only to avoid overfitting
+Result.A_D_AA <- mmer(y~1,
+                    random = ~vsr(id, Gu = A) +
+                      vsr(idd, Gu = D) +
+                      vsr(idaa, Gu = AA),
+                    rcov = ~ units,
+                    data = pheno_dat)
+
+# Make sure we have some results
+if(!exists("Result.A_D_AA")) {
+    result <- data.frame(gen = run_info[1],
+                            seed = as.character(run_info[2]),
+                            modelindex = run_info[3],
+                            VarA = 0, 
+                            VarD = 0,
+                            VarAA = 0,
+                            VarR = 0,
+                            VarA.SE = 0,
+                            VarD.SE = 0,
+                            VarAA.SE = 0,
+                            VarR.SE = 0,
+                            H2.A = 0,
+                            H2.D = 0,
+                            H2.AA = 0,
+                            AIC = 0
+                            )
+    write.table(result, paste0("/scratch/ht96/nb9894/h2/getH2/out_h2_", run_chunk, ".csv"), sep = ",", row.names = F, col.names = F)    q(save = "no")
+    q(save = "no")
+}
+
+#Extract results
+Result.A_D_AA.Extract <- data.frame(gen = run_info[1],
+                            seed = as.character(run_info[2]),
+                            modelindex = run_info[3],
+                            VarA = unname(Result.A_D_AA$sigma[[1]]), #Variance components
+                            VarD = unname(Result.A_D_AA$sigma[[2]]),
+                            VarAA = unname(Result.A_D_AA$sigma[[3]]),
+                            VarR = unname(Result.A_D_AA$sigma[[4]]),
+                            VarA.SE = sqrt(diag(Result.A_D_AA$sigmaSE))[1],#SE of variance components
+                            VarD.SE = sqrt(diag(Result.A_D_AA$sigmaSE))[2],
+                            VarAA.SE = sqrt(diag(Result.A_D_AA$sigmaSE))[3],
+                            VarR.SE = sqrt(diag(Result.A_D_AA$sigmaSE))[4],
+                            H2.A = vpredict(Result.A_D_AA, h2 ~ V1 / (V1+V2+V3+V4)),#Proportions
+                            H2.D = vpredict(Result.A_D_AA, h2 ~ V2 / (V1+V2+V3+V4)),
+                            H2.AA = vpredict(Result.A_D_AA, h2 ~ V3 / (V1+V2+V3+V4)),
+                            AIC = Result.A_D_AA$AIC
+                            )
+######################################################################
+
+rownames(Result.A_D_AA.Extract) <- NULL
+
+# Output file
+write.table(Result.A_D_AA.Extract, paste0("/scratch/ht96/nb9894/h2/getH2/out_h2_", run_chunk, ".csv"), sep = ",", row.names = F, col.names = F)
