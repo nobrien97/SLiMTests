ad4f4fe0c6c415362a38bc595b16925029a5b123
nobrien97 <nicholasobrien97@gmail.com>
2022-11-24 19:17:50 +1000

    updated combos code getH2_hsfs

--- tests/h2_hsfs/getH2_hsfs/R/combos.R
+++ tests/h2_hsfs/getH2_hsfs/R/combos.R
@@ -2,11 +2,11 @@
 filename <- "./getH2_hsfsSR.sh"
 
 # Run numbers to extract haplotypes
-run <- 0:29519
+run <- 0:59039
 
 # Chunk numbers to collect output: from 1 to 15, which is 1968 files per chunk
 
-chunk <- 1:15
+chunk <- 1:30
 
 cmd_df <- data.frame(
     filename = filename,
