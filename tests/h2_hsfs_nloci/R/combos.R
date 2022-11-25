463a75fb88dbe3ee89a474c7395fd220e8ae4f60
nobrien97 <nicholasobrien97@gmail.com>
2022-11-25 16:27:36 +1000

    updated cmds with more seeds

--- tests/h2_hsfs_nloci/R/combos.R
+++ tests/h2_hsfs_nloci/R/combos.R
@@ -1,8 +1,8 @@
 # Generate cmds.txt
 singleRunBashName <- "./h2_hsfs_nlociSR.sh"
-seeds <- 1:24
+seeds <- 1:48
 path <- "/mnt/c/GitHub/SLiMTests/tests/h2_hsfs_nloci/R/h2_hsfs_nloci_seeds.csv"
-system(paste0("SeedGenerator -n 24 -t -d ", path))
+system(paste0("SeedGenerator -n 48 -t -d ", path))
 model <- 0:1
 nloci <- c(10, 100, 1000)
 width <- c(0.05, 0.105, 0.22)
