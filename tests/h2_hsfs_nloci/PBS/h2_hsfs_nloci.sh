96d6a4f4089ca7bc7e0ffe832de278a5d25c5981
nobrien97 <nicholasobrien97@gmail.com>
2022-11-25 16:14:58 +1000

    Updated hsfs_nloci to a self-submitting job

--- tests/h2_hsfs_nloci/PBS/h2_hsfs_nloci.sh
+++ tests/h2_hsfs_nloci/PBS/h2_hsfs_nloci.sh
@@ -8,15 +8,40 @@
   
   
 ECHO=/bin/echo
+#
+# These variables are assumed to be set:
+#   NJOBS is the total number of jobs in a sequence of jobs (defaults to 1)
+#   NJOB is the number of the current job in the sequence (defaults to 0)
+#
+  
+if [ X$NJOBS == X ]; then
+    $ECHO "NJOBS (total number of jobs in sequence) is not set - defaulting to 1"
+    export NJOBS=1
+fi
+  
+if [ X$NJOB == X ]; then
+    $ECHO "NJOB (current job number in sequence) is not set - defaulting to 0"
+    export NJOB=0
+    # Since this is the first iteration, create our folders
+    $ECHO "Creating outputs folders..."
+    cd $PBS_O_WORKDIR
 
-$ECHO "Creating outputs folders..."
-# create our folders
-cd $PBS_O_WORKDIR
+    # Make output folder
+    mkdir /scratch/ht96/nb9894/h2_hsfs_nloci
+    mkdir /g/data/ht96/nb9894/h2_hsfs_nloci
+    mkdir $HOME/tests/h2_hsfs_nloci/done
 
-# Make output folder
-mkdir /scratch/ht96/nb9894/h2_hsfs_nloci
-mkdir /g/data/ht96/nb9894/h2_hsfs_nloci
-mkdir $HOME/tests/h2_hsfs_nloci/done
+fi
+
+#
+# Quick termination of job sequence - look for a specific file 
+#
+if [ -f STOP_SEQUENCE ] ; then
+    $ECHO  "Terminating sequence at job number $NJOB"
+    exit 0
+fi
+
+$ECHO "Starting job $NJOB of $NJOBS"
 
 # Pre-job file manipulation goes here ...
 # 
@@ -40,8 +65,17 @@ export ncores_per_numanode=12
 # Calculate the range of parameter combinations we are exploring this job
 # CAUTION: may error if CUR_TOT is not a multiple of PBS_NCPUS - untested
 CMDS_PATH=$HOME/tests/h2_hsfs_nloci/PBS/cmds.txt
+CUR_TOT=$(cat $CMDS_PATH | wc -l)
+CUR_MIN=$(($NJOB*$PBS_NCPUS+1))
+CUR_MAX=$((($NJOB+1)*$PBS_NCPUS))
 
-mpirun -np $((PBS_NCPUS/ncores_per_task)) --map-by ppr:$((ncores_per_numanode/ncores_per_task)):NUMA:PE=${ncores_per_task} nci-parallel --input-file ${CMDS_PATH} --timeout 172800
+if [ $CUR_MAX -gt $CUR_TOT ]; then
+    CUR_MAX=$CUR_TOT
+fi
+
+sed -n -e "${CUR_MIN},${CUR_MAX}p" $CMDS_PATH > ./JOB_PATH.txt
+
+mpirun -np $((PBS_NCPUS/ncores_per_task)) --map-by ppr:$((ncores_per_numanode/ncores_per_task)):NUMA:PE=${ncores_per_task} nci-parallel --input-file ./JOB_PATH.txt --timeout 172800
 
 $ECHO "All jobs finished, moving output..."
 
@@ -66,3 +100,30 @@ cat ./slim_time* >> $SAVEDIR/slim_time.csv
 # Delete loose files with seed and model indices
 find -regex ".*[0-9]*_*[0-9].csv+" -delete
 rm *.tsv
+
+# 
+# Check the exit status
+#
+errstat=$?
+if [ $errstat -ne 0 ]; then
+    # A brief nap so PBS kills us in normal termination
+    # If execution line above exceeded some limit we want PBS
+    # to kill us hard
+    sleep 5 
+    $ECHO "Job number $NJOB returned an error status $errstat - stopping job sequence."
+    exit $errstat
+fi
+
+#   
+# Are we in an incomplete job sequence - more jobs to run ?
+#   
+if [ $NJOB -lt $NJOBS ]; then
+# Now increment counter and submit the next job
+# 
+    NJOB=$(($NJOB+1))
+    $ECHO "Submitting job number $NJOB in sequence of $NJOBS jobs"
+    cd $PBS_O_WORKDIR
+    qsub -v NJOBS=$NJOBS,NJOB=$NJOB ./NAR_QG_Exp.sh
+else
+    $ECHO "Finished last job in sequence of $NJOBS jobs"
+fi
