1600d0eaf239cddaec114aaf69e34c2b93215c37
nobrien97 <nicholasobrien97@gmail.com>
2022-12-08 17:22:17 +1000

    Started results from nloci

--- tests/h2_hsfs_nloci/getH2_hsfs_nloci/R/ploth2.R
+++ tests/h2_hsfs_nloci/getH2_hsfs_nloci/R/ploth2.R
@@ -1,6 +1,9 @@
 # Plot heritability estimates
 library(tidyverse)
+library(grid)
 library(gridExtra)
+library(latex2exp)
+library(cowplot)
 
 se <- function(x) {
   return(sd(x)/sqrt(length(x)))
@@ -19,20 +22,24 @@ d_h2 %>% distinct() -> d_h2
 # Remove NAs
 d_h2 %>% drop_na() -> d_h2
 
-# Recode modelindex to additive/network
-d_h2 %>% mutate(model = if_else(as.logical(modelindex %% 2), "Network", "Additive")) -> d_h2
-d_h2$model <- as.factor(d_h2$model)
+d_h2 %>% filter(VarA >= 0, VarA < 10,
+                VarD >= 0, VarAA >= 0,
+                VarR > 0, sum(VarA, VarD, VarAA, VarR) > 0) -> d_h2
+
 
 # Add our other variables
 combos <- read_delim("../../R/combos.csv", delim = " ", col_names = F)
 names(combos) <- c("model", "nloci", "width", "locisigma")
-d_h2 %>% mutate(nloci = combos$nloci[.$modelindex],
+d_h2 %>% mutate(model = combos$model[.$modelindex],
+                nloci = combos$nloci[.$modelindex],
                 width = combos$width[.$modelindex],
                 sigma = combos$locisigma[.$modelindex]) -> d_h2
 
+# Recode modelindex to additive/network
+d_h2 %>% mutate(model = recode_factor(model, `0`="Additive", `1`="Network")) -> d_h2
 
 d_h2_sum <- d_h2 %>%
-  group_by(gen, model) %>% 
+  group_by(gen, model, nloci, sigma) %>% 
   summarise(meanH2A = mean(H2.A.Estimate),
             seH2A = se(H2.A.Estimate),
             meanH2D = mean(H2.D.Estimate), 
@@ -56,59 +63,117 @@ ggplot(d_h2, aes(x = H2.A.Estimate, fill = model)) +
 
 
 # Mean over time
-ggplot(d_h2_sum, aes(x = gen, y = meanH2A, color = model)) +
+h2A_time <- ggplot(d_h2_sum %>% filter(nloci != 100, sigma != 0.01), aes(x = gen, y = meanH2A, color = model)) +
+  facet_grid(nloci~sigma) +
   geom_line() +
+  scale_y_continuous(limits = c(0, 1)) +
   scale_fill_discrete(guide = "none") +
   geom_ribbon(aes(ymin = (meanH2A - seH2A), ymax = (meanH2A + seH2A), 
                   fill = model), color = NA, alpha = 0.2) +
-  labs(x = "Generations after optimum shift", y = "Average narrow-sense heritability", color = "Model") +
+  labs(x = "Generations after optimum shift", y = "Mean heritability", color = "Model") +
   theme_bw() +
-  theme(text=element_text(size=20))
+  theme(text=element_text(size=16))
 
 # What about other variance components?
-ggplot(d_h2_sum, aes(x = gen, y = meanH2D, color = model)) +
+h2D_time <- ggplot(d_h2_sum %>% filter(nloci != 100, sigma != 0.01), aes(x = gen, y = meanH2D, color = model)) +
+  facet_grid(nloci~sigma) +
+  scale_y_continuous(limits = c(0, 1)) +
   geom_line() +
   scale_fill_discrete(guide = "none") +
   geom_ribbon(aes(ymin = (meanH2D - seH2D), ymax = (meanH2D + seH2D), 
                   fill = model), color = NA, alpha = 0.2) +
   labs(x = "Generations after optimum shift", y = "Average dominance variance proportion", color = "Model") +
   theme_bw() +
-  theme(text=element_text(size=20))
+  theme(text=element_text(size=16))
 
 
-ggplot(d_h2_sum, aes(x = gen, y = meanH2AA, color = model)) +
+h2AA_time <- ggplot(d_h2_sum %>% filter(nloci != 100, sigma != 0.01), aes(x = gen, y = meanH2AA, color = model)) +
+  facet_grid(nloci~sigma) +
+  scale_y_continuous(limits = c(0, 1)) +
   geom_line() +
   scale_fill_discrete(guide = "none") +
   geom_ribbon(aes(ymin = (meanH2AA - seH2AA), ymax = (meanH2AA + seH2AA), 
                   fill = model), color = NA, alpha = 0.2) +
   labs(x = "Generations after optimum shift", y = "Average AxA variance proportion", color = "Model") +
   theme_bw() +
-  theme(text=element_text(size=20))
+  theme(text=element_text(size=16))
+
+plt_row <- plot_grid(
+  h2A_time + theme(legend.position = "none", panel.spacing = unit(1, "lines")) + 
+    xlab(NULL) + draw_label(TeX("$h^2_{A}$"), x = 1800, y = 0.975),
+  h2D_time + theme(legend.position = "none", panel.spacing = unit(1, "lines")) + 
+    ylab(NULL) + xlab(NULL) +
+    draw_label(TeX("$h^2_{D}$"), x = 1800, y = 0.975),
+  h2AA_time + theme(legend.position = "none", panel.spacing = unit(1, "lines")) + 
+    ylab(NULL) + xlab(NULL) +
+    draw_label(TeX("$h^2_{AA}$"), x = 1800, y = 0.975),
+  nrow = 1
+)
+
+x.grob <- textGrob("Generations after optimum shift", 
+                   gp = gpar(fontsize = 16))
+
+y.grob <- textGrob("Number of QTLs", 
+                   gp = gpar(fontsize = 16), rot=270)
+
+top.grob <- textGrob("Mutational effect size variance", 
+                     gp = gpar(fontsize = 16))
+
+g <- arrangeGrob(plt_row, bottom = x.grob, right = y.grob, top = top.grob)
+h2_leg <- get_legend(h2A_time)
+
+plt_row_leg <- plot_grid(g, h2_leg, rel_widths = c(3, .4))
+plt_row_leg
+
+ggsave("h2_time.png", plt_row_leg,  height = 6, width = 18, bg = "white")
 
 
+###################################################
 # stacked percent bar chart
-ggplot(d_h2_sum %>% pivot_longer(
-  cols = c(meanH2A, meanH2D, meanH2AA),
-  names_to = "varComp", values_to = "prop"), 
-  aes(x = gen, y = prop, fill = varComp)) +
+h2_stk_var0.1 <- ggplot(d_h2_sum %>% filter(nloci != 100, sigma == 0.1) %>% 
+                          pivot_longer(cols = c(meanH2A, meanH2D, meanH2AA),
+                                       names_to = "varComp", values_to = "prop"), 
+                        aes(x = gen, y = prop, fill = varComp)) +
   scale_fill_viridis_d(labels = c("Additive", "Epistatic (AxA)", "Dominant")) +
-  facet_grid(.~model) +
-  geom_bar(position="fill", stat="identity", width = 50) +
-  labs(x = "Generations after optimum shift", y = "Proportion of total phenotypic variance", 
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1),
+                     sec.axis = sec_axis(~ ., name = "Number of QTLs", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Model", 
+                                         breaks = NULL, labels = NULL)) +
+  facet_grid(nloci~model) +
+  geom_bar(position="stack", stat="identity", width = 50) +
+  labs(x = "Generations after optimum shift", y = "Proportion of total\nphenotypic variance", 
        fill = "Variance component") +
   theme_bw() +
-  theme(text = element_text(size = 20))
+  theme(text = element_text(size = 16), legend.position = "bottom")
 
-ggplot(d_h2_sum %>% pivot_longer(
-  cols = c(meanVarA, meanVarD, meanVarAA),
-  names_to = "varComp", values_to = "var"),
-  aes(x = gen, y = var)) +
-  scale_color_viridis_d() +
-  facet_grid(varComp~model) +
-  geom_line() +
-  labs(x = "Generations after optimum shift", y = "Variance") +
+h2_stk_var1 <- ggplot(d_h2_sum %>% filter(nloci != 100, sigma == 1) %>% 
+                        pivot_longer(cols = c(meanH2A, meanH2D, meanH2AA),
+                                     names_to = "varComp", values_to = "prop"), 
+                      aes(x = gen, y = prop, fill = varComp)) +
+  scale_fill_viridis_d(labels = c("Additive", "Epistatic (AxA)", "Dominant")) +
+  scale_y_continuous(labels = scales::percent, limits = c(0,1),
+                     sec.axis = sec_axis(~ ., name = "Number of QTLs", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Model", 
+                                         breaks = NULL, labels = NULL)) +
+  facet_grid(nloci~model) +
+  geom_bar(position="stack", stat="identity", width = 50) +
+  labs(x = "Generations after optimum shift", y = "Proportion of total\nphenotypic variance", 
+       fill = "Variance component") +
   theme_bw() +
-  theme(text = element_text(size = 20))
+  theme(text = element_text(size = 16), legend.position = "bottom")
+
+h2_leg <- get_legend(h2_stk_var0.1)
+
+plt_stk <- plot_grid(h2_stk_var0.1 + theme(legend.position = "none"),
+                     h2_stk_var1 + theme(legend.position = "none"),
+                     labels = "AUTO")
+
+plt_stk <- plot_grid(plt_stk, h2_leg, ncol = 1, rel_heights = c(1, .1))
+plt_stk
+
+ggsave("h2_stacked.png", plt_stk, width = 20, height = 5, bg = "white")
 
 
 # Plot trait data over time
@@ -118,9 +183,47 @@ names(d_qg) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean",
 d_com <- inner_join(d_h2, d_qg, by = c("gen", "seed", "modelindex"))
 write_csv(d_com, "d_h2_qg.csv")
 
+d_qg %>% filter(phenomean < 100) -> d_qg
+
+d_qg %>% mutate(model = combos$model[.$modelindex],
+                nloci = combos$nloci[.$modelindex],
+                width = combos$width[.$modelindex],
+                sigma = combos$locisigma[.$modelindex]) -> d_qg
+
+# Recode modelindex to additive/network
+d_qg %>% mutate(model = recode_factor(model, `0`="Additive", `1`="Network")) -> d_qg
+
+
+
+d_qg_sum <- d_qg %>% group_by(gen, model, nloci, sigma) %>%
+  summarise(He = mean(meanH),
+            seHe = se(meanH),
+            meanPheno = mean(phenomean),
+            sePheno = se(phenomean),
+            meanDist = mean(dist),
+            seDist = se(dist))
+
+ggplot(d_qg_sum %>% filter(nloci < 100, sigma != 0.01), 
+       aes(gen, meanPheno, color = model)) +
+  facet_grid(nloci~sigma) +
+  geom_line() +
+  geom_hline(yintercept = 2, linetype = "dashed") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Number of QTLs", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Mutational effect variance", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_fill_discrete(guide = "none") +
+  geom_ribbon(aes(ymin = (meanPheno - sePheno), ymax = (meanPheno + sePheno), 
+                  fill = model), color = NA, alpha = 0.2) +
+  labs(x = "Generations after optimum shift", y = "Mean of population mean phenotypes", color = "Model") +
+  theme_bw() +
+  theme(text = element_text(size=20), panel.spacing = unit(1, "lines"))
+
+
+
 d_com <- d_com %>% filter(phenomean < 10)
 
-d_com_sum <- d_com %>% group_by(gen, model) %>%
+d_com_sum <- d_com %>% group_by(gen, model, nloci, sigma) %>%
   summarise(He = mean(meanH),
             seHe = se(meanH),
             meanPheno = mean(phenomean),
@@ -130,45 +233,27 @@ d_com_sum <- d_com %>% group_by(gen, model) %>%
 
 d_com_sum$gen <- d_com_sum$gen - 50000
 
-ggplot(d_com_sum, aes(gen, meanPheno, color = model)) +
+ggplot(d_com_sum %>% filter(nloci != 100, sigma != 0.01), aes(gen, meanPheno, color = model)) +
+  facet_grid(nloci~sigma) +
   geom_line() +
+  geom_hline(yintercept = 2, linetype = "dashed") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Number of QTLs", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Mutational effect variance", 
+                                         breaks = NULL, labels = NULL)) +
   scale_fill_discrete(guide = "none") +
   geom_ribbon(aes(ymin = (meanPheno - sePheno), ymax = (meanPheno + sePheno), 
                   fill = model), color = NA, alpha = 0.2) +
   labs(x = "Generations after optimum shift", y = "Mean of population mean phenotypes", color = "Model") +
   theme_bw() +
-  theme(text = element_text(size=20))
+  theme(text = element_text(size=20), panel.spacing = unit(1, "lines"))
+
+ggsave("pheno_time.png", width = 8, height = 6, bg = "white")
 
 # Mutation data
-d_com <- read_csv("../data/d_combined_after_small.csv")
+d_com <- read_csv("../data/d_com_sum.csv")
 
-# Plot SFS per h2 per group
-d_com <- d_com %>% filter(gen < 52000)
-d_com_end <- d_com %>% filter(gen > 51000)
+# Plot mean freq and value
 
-combos <- read_delim("../../R/combos.csv", delim = " ", col_names = F)
-names(combos) <- c("model", "nloci", "width", "locisigma")
-d_com_end %>% mutate(nloci = combos$nloci[.$modelindex],
-                width = combos$width[.$modelindex],
-                sigma = combos$locisigma[.$modelindex],
-                h2A = cut(H2.A.Estimate, breaks = c(0, 0.25, 0.5, 0.75, 1)),
-                h2D = cut(H2.D.Estimate, breaks = c(0, 0.25, 0.5, 0.75, 1)),
-                h2AA = cut(H2.AA.Estimate, breaks = c(0, 0.25, 0.5, 0.75, 1)),
-                fixTime = fixGen - originGen) -> d_com_end
-
-
-sfs_plots <- by(d_com_end, d_com_end$h2AA, function(sub){
-  ggplot(sub %>% group_by(model, mutType) %>%
-           mutate(weight = 1 / n()), 
-         aes(x = fixTime)) +
-    facet_grid(model~mutType) +
-    geom_histogram(aes(weight = weight), bins = 10) +
-    scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
-    labs(y = "Relative frequency") +
-    ggtitle(sub$h2AA[[1]]) +
-    theme_bw() + theme(plot.title = element_text(hjust = 0.5))
-})
-
-grid.arrange(grobs = sfs_plots, ncol = 2)
-
+ggplot(d_com %>% nloci != 100)
 
--- tests/h2_hsfs_nloci/getH2_hsfs_nloci/README.md
+++ tests/h2_hsfs_nloci/getH2_hsfs_nloci/README.md
@@ -3,3 +3,18 @@
 This job calculates heritability and variance components using sommer mmer functions, using the data from the h2_hsfs_nloci slim experiment. 
 
 wc -l slim_haplos.csv -> 301104000/2000 = 150552 samples
+
+## Results
+
+I started by exploring the effects of each variable on heritability. The selection strength didn't have much of an effect on any heritability (A, D, or AxA) so averaged over it. I also removed the middle QTL number because it was really just an intermediate effect between the extremes (10 and 1000). The same was true with the smallest effect size, which behaved similarly to $\sigma_{loci} = 0.1.
+
+The first result is the heritability over time: additive, dominance and AxA epistatic
+
+![](./R/h2_time.png)
+
+The network and additive models behave quite similarly when there are few loci, equilibriating to a similar additive heritability by generation 2000. With many loci, they perform quite differently: network models have very low additivity whereas additive models harbour much greater proportions of additive variance. With small effects, additive models maintain heritability, possibly due to a drift barrier that precludes adaptation, or due to balancing selection that maintains variability. When effects are large, the additive model's heritability decreases as standing variation is used by adaptation. This case is true for both levels of mutational variance in the network model, although with small effects the network model starts with considerably higher $h^2_A$. Over time, $h^2_{AA}$ increases in the network model: it decreases again under small effects, but remains steady (but stochastic) with larger mutational effects. Interestingly, as the additive model uses standing variation and $h^2_A$ decreases, $h^2_D$ increases at a similar rate. Is this evidence of overdominance?
+
+Another way to view this is with a stacked bar chart: 
+![](./R/h2_stacked.png)
+
+White space is residual variance. With this view, it is easier to see the difference between additive and network models when there are few loci and small effects (panel A). A small, but reasonable consistent amount of AxA variance contributes to the trait in the network case, and the linear model is able to find more additive variance than in the additive case. This is likely because the network model has larger genetic effects for the same molecular effect size owing to the potential for interactions to enhance genetic effects: the epistatic variance is able to be converted to additive variance during adaptation (which might happen: the amount of epistatic variance does slightly decrease over time).
