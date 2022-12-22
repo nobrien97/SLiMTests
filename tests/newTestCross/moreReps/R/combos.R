8eed1eed2ed38e6164483e46fb2381a886cf9eb7
nobrien97 <nicholasobrien97@gmail.com>
2022-12-22 15:08:51 +1000

    Job for more replicates for newTestCross

---
+++ tests/newTestCross/moreReps/R/combos.R
@@ -0,0 +1,17 @@
+# Generate cmds.txt
+singleRunBashName <- "./newTestCrossSR.sh"
+seeds <- 1:48
+path <- "/mnt/c/GitHub/SLiMTests/tests/newTestCross/moreReps/R/newTestCross_seeds.csv"
+system(paste0("SeedGenerator -n 48 -t -d ", path))
+molTrait <- c(-1, 0:3)
+nloci <- c(8, 98, 998)
+locisigma <- c(0.1, 1)
+
+dfCMDs <- expand.grid(molTrait, nloci, locisigma)
+write.table(dfCMDs, "/mnt/c/GitHub/SLiMTests/tests/newTestCross/moreReps/R/combos.csv", row.names = F, col.names = F)
+
+cmds <- data.frame(sr = rep(singleRunBashName, times = nrow(dfCMDs)),
+                   seed = rep(seeds, times = nrow(dfCMDs)),
+                   model = rep(1:nrow(dfCMDs), each = length(seeds)))
+
+write.table(cmds, "/mnt/c/GitHub/SLiMTests/tests/newTestCross/moreReps/PBS/cmds.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
---
+++ tests/newTestCross/moreReps/getH2_newTestCross/R/combos.R
@@ -0,0 +1,21 @@
+# File name
+filename <- "./getH2_newTestCrossSR.sh"
+
+#  wc -l slim_haplo.csv -> 118080000/2000 haplotypes = 59040 samples
+
+
+# Run numbers to extract haplotypes
+run <- 0:59039
+
+# Chunk numbers to collect output: from 1 to 30, which is 1968 files per chunk
+
+chunk <- 1:30
+
+cmd_df <- data.frame(
+    filename = filename,
+    run = run,
+    chunk = rep(chunk, each = 1968),
+    haplos = rep(0:7, each = 7380)
+)
+
+write.table(cmd_df, "/mnt/c/GitHub/SLiMTests/tests/newTestCross/getH2_newTestCross/PBS/cmds.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
