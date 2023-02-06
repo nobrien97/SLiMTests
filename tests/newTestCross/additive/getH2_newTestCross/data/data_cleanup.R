ef10dc87c42c6d0a3cff8cbf13cc635aac13727d
nobrien97 <nicholasobrien97@gmail.com>
2023-02-06 13:50:53 +1000

    Added estimated response to selection

--- tests/newTestCross/additive/getH2_newTestCross/data/data_cleanup.R
+++ tests/newTestCross/additive/getH2_newTestCross/data/data_cleanup.R
@@ -1,6 +1,14 @@
 
 library(tidyverse)
 
+se <- function(x, na.rm = F) {
+  if (na.rm)
+    x <- x[!is.na(x)]
+  
+  return(sd(x)/sqrt(length(x)))
+}
+
+
 path <- "/mnt/d/SLiMTests/tests/newTestCross/additive/"
 
 # Read in data
@@ -34,6 +42,7 @@ d_h2$modelindex <- as.factor(d_h2$modelindex)
 width_condensed <- 0.05
 width_sqrd <- 1/(width_condensed * 2)
 opt <- 2
+TIME_BETWEEN_SAMPLES <- 50 # 50 generations between each sample
 
 d_phenos <- read_csv(paste0(path, "slim_sampled_pheno.csv"), col_names = F)
 d_phenos <- d_phenos %>% pivot_longer(cols = 4:ncol(d_phenos), names_to = NULL, values_to = "phenotype")
@@ -51,13 +60,6 @@ calcBeta <- function(S, mu, theta) {
 }
 
 # Use Morrissey and Goudie 2022 to predict beta
-library(future)
-library(foreach)
-library(doParallel)
-
-cl <- makeCluster(future::availableCores())
-registerDoParallel(cl)
-
 d_h2$beta <- NA
 
 d_h2_pheno <- inner_join(d_h2, d_phenos, c("gen", "seed", "modelindex"))
@@ -67,7 +69,7 @@ d_h2_pheno <- d_h2_pheno %>%
   mutate(S = 1/(width_sqrd + var(phenotype)),
          beta = calcBeta(S, mean(phenotype), opt))
 
-d_h2_pheno$estR <- d_h2$H2.A.Estimate * d_h2_pheno$beta
+d_h2_pheno$estR <- (d_h2$H2.A.Estimate * d_h2_pheno$beta) * TIME_BETWEEN_SAMPLES
 
 d_h2_pheno <- d_h2_pheno %>%
   distinct(gen, seed, modelindex, .keep_all = T) %>%
@@ -75,94 +77,172 @@ d_h2_pheno <- d_h2_pheno %>%
 
 # Combine the data frames
 d_combined <- inner_join(d_muts, d_qg, by = c("gen", "seed", "modelindex"))
-d_combined_full <- full_join(d_combined, d_h2, by = c("gen", "seed", "modelindex"))
-
+rm(d_muts, d_qg)
+d_combined <- full_join(d_combined, d_h2_pheno, by = c("gen", "seed", "modelindex"))
+rm(d_h2_pheno)
 # Deviation of observed R to estimated R
-d_combined_full$devEstR <- c(0, d_combined_full[2:nrow(d_combined_full),"phenomean"] - d_combined_full$devEstR)
+d_combined$devEstR <- d_combined$deltaPheno - d_combined$estR
 
 # View the combined data and write to file
-head(d_combined_full) 
-colnames(d_combined_full)
-d_combined_full[1,] %>% as_tibble() %>% print(width=Inf)
-d_combined_full[800000,] %>% as_tibble() %>% print(width=Inf)
-data.table::fwrite(d_combined_full, "d_combined_full.csv")
+head(d_combined) 
+colnames(d_combined)
+d_combined[1,] %>% as_tibble() %>% print(width=Inf)
+d_combined[800000,] %>% as_tibble() %>% print(width=Inf)
+data.table::fwrite(d_combined, paste0(path, "getH2_newTestCross/data/d_combined_full.csv"))
 
 # Write only the data after burn-in
-d_combined_after <- d_combined_full %>% filter(gen >= 50000)                                                                       
+d_combined_after <- d_combined %>% filter(gen >= 50000)                                                                       
 nrow(d_combined_after)
 nrow(d_combined_full)
-data.table::fwrite(d_combined_after, "d_combined_after.csv")
+data.table::fwrite(d_combined_after, paste0(path, "getH2_newTestCross/data/d_combined_after.csv"))
 
 
-# Write summary data for mean frequencies
+combos <- read_delim("../../R/combos.csv", delim = " ", col_names = F)
+names(combos) <- c("nloci", "locisigma")
+#d_combined_after <- read_csv(paste0(path, "getH2_newTestCross/data/d_combined_after.csv"))
+d_combined_after %>% mutate(
+                            nloci = combos$nloci[.$modelindex],
+                            sigma = combos$locisigma[.$modelindex]) -> d_combined_after
 
-d_com_end %>% filter(fixedEffect == -1) %>% mutate(h2A = cut(H2.A.Estimate, breaks = c(0, 0.25, 0.5, 0.75, 1), include.lowest = T),
-                          h2D = cut(H2.D.Estimate, breaks = c(0, 0.25, 0.5, 0.75, 1), include.lowest = T),
-                          h2AA = cut(H2.AA.Estimate, breaks = c(0, 0.25, 0.5, 0.75, 1), include.lowest = T),
-                          fixTime = fixGen - originGen) -> d_com_end_ctrl
 
-d_com_end_ctrl %>% mutate(nloci = combos$nloci[.$modelindex],
-                          sigma = combos$locisigma[.$modelindex],
-                          molTrait = recode_factor(mutType, `1`="Neutral", `2`="Del", `3`="\u03B1", `4`="\u03B2", `5`="KZ", `6`="KXZ")) -> d_com_end_ctrl
 
+# Write summary data for mean frequencies, effect sizes, and estimated responses to selection
+labs <- c("[0,0.25]", "(0.25, 0.5]", "(0.5,0.75]", "(0.75,1]")
+d_combined_after %>% mutate(h2A = cut(H2.A.Estimate, breaks = c(-Inf, 0.25, 0.5, 0.75, Inf), labels = labs, include.lowest = T, ordered_result = T),
+                                                          h2D = cut(H2.D.Estimate, breaks = c(-Inf, 0.25, 0.5, 0.75, Inf), labels = labs, include.lowest = T, ordered_result = T),
+                                                          h2AA = cut(H2.AA.Estimate, breaks = c(-Inf, 0.25, 0.5, 0.75, Inf), labels = labs, include.lowest = T, ordered_result = T),
+                                                          fixTime = fixGen - originGen) -> d_com_end_ctrl
 
 d_com_ctrl_h2A_sum <- d_com_end_ctrl %>%
   select(-c(fixGen, fixTime)) %>%
   drop_na() %>%
-  group_by(nloci, molTrait, h2A) %>%
+  group_by(nloci, sigma, h2A) %>%
   summarise(meanFreq = mean(Freq),
             seFreq = se(Freq),
             meanFX = mean(value), 
-            seFX = se(value)
+            seFX = se(value),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
   )
 
 d_com_ctrl_h2D_sum <- d_com_end_ctrl %>%
   select(-c(fixGen, fixTime)) %>%
   drop_na() %>%
-  group_by(nloci, molTrait, h2D) %>%
+  group_by(nloci, sigma, h2D) %>%
   summarise(meanFreq = mean(Freq),
             seFreq = se(Freq),
             meanFX = mean(value), 
-            seFX = se(value)
+            seFX = se(value),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
   )
 
 d_com_ctrl_h2AA_sum <- d_com_end_ctrl %>%
   select(-c(fixGen, fixTime)) %>%
   drop_na() %>%
-  group_by(nloci, molTrait, h2AA) %>%
+  group_by(nloci, sigma, h2AA) %>%
   summarise(meanFreq = mean(Freq),
             seFreq = se(Freq),
             meanFX = mean(value), 
-            seFX = se(value)
+            seFX = se(value),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
   )
-
-# include stuff for molecular trait
-
-d_com_end_ctrl %>% pivot_longer(c(aZ, bZ, KZ, KXZ), names_to = "molTraitVal_name", 
-                            values_to = "molTraitVal_value") -> d_com_end_ctrl_molTraitVal
 
 d_com_ctrl_sum <- d_com_end_ctrl %>%
   select(-c(fixGen, fixTime)) %>%
   drop_na() %>%
-  group_by(nloci, molTrait, h2A, h2D, h2AA) %>%
+  group_by(nloci, sigma, h2A, h2D, h2AA) %>%
   summarise(meanFreq = mean(Freq),
             seFreq = se(Freq),
             meanFX = mean(value), 
-            seFX = se(value)
+            seFX = se(value),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
   )
 
-d_com_ctrl_molTraitVal_sum <- d_com_end_ctrl_molTraitVal %>%
+# Response to selection
+d_com_resp_h2A <- d_com_end_ctrl %>%
   select(-c(fixGen, fixTime)) %>%
   drop_na() %>%
-  group_by(nloci, molTraitVal_name, h2A, h2D, h2AA) %>%
-  summarise(meanMolTraitVal = mean(molTraitVal_value),
-            seMolTraitVal = se(molTraitVal_value)
-  )
-data.table::fwrite(d_com_ctrl_molTraitVal_sum, "d_com_sum_molTraitVal.csv")
+  group_by(gen, nloci, h2A) %>%
+  summarise(meanPheno = mean(phenomean),
+            sePheno = se(phenomean),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
+  ) %>%
+  ungroup() %>%
+  group_by(nloci, h2A) %>%
+  mutate(obsPheno = cumsum(meanDelta),
+         expPheno = cumsum(meanEstResp),
+         errPheno = cumsum(meanDeltaEst))
+
+data.table::fwrite(d_com_resp_h2A, paste0(path, "getH2_newTestCross/data/d_com_resp_h2A.csv"))
+
+d_com_resp_h2D <- d_com_end_ctrl %>%
+  select(-c(fixGen, fixTime)) %>%
+  drop_na() %>%
+  group_by(gen, nloci, h2D) %>%
+  summarise(meanPheno = mean(phenomean),
+            sePheno = se(phenomean),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
+  ) %>%
+  ungroup() %>%
+  group_by(nloci, h2D) %>%
+  mutate(obsPheno = cumsum(meanDelta),
+         expPheno = cumsum(meanEstResp),
+         errPheno = cumsum(meanDeltaEst))
+
+data.table::fwrite(d_com_resp_h2D, paste0(path, "getH2_newTestCross/data/d_com_resp_h2D.csv"))
+
+d_com_resp_h2AA <- d_com_end_ctrl %>%
+  select(-c(fixGen, fixTime)) %>%
+  drop_na() %>%
+  group_by(gen, nloci, h2AA) %>%
+  summarise(meanPheno = mean(phenomean),
+            sePheno = se(phenomean),
+            meanEstResp = mean(estR),
+            seEstResp = se(estR),
+            meanDelta = mean(deltaPheno),
+            seDelta = se(deltaPheno),
+            meanDeltaEst = mean(devEstR),
+            seDeltaEst = se(devEstR)
+  ) %>%
+  ungroup() %>%
+  group_by(nloci, h2AA) %>%
+  mutate(obsPheno = cumsum(meanDelta),
+         expPheno = cumsum(meanEstResp),
+         errPheno = cumsum(meanDeltaEst))
+
+data.table::fwrite(d_com_resp_h2AA, paste0(path, "getH2_newTestCross/data/d_com_resp_h2AA.csv"))
 
 
-
-data.table::fwrite(d_com_ctrl_h2A_sum, "d_com_sum_h2A.csv")
-data.table::fwrite(d_com_ctrl_h2D_sum, "d_com_sum_h2D.csv")
-data.table::fwrite(d_com_ctrl_h2AA_sum, "d_com_sum_h2AA.csv")
-data.table::fwrite(d_com_ctrl_sum, "d_com_sum.csv")
+data.table::fwrite(d_com_ctrl_h2A_sum, paste0(path, "getH2_newTestCross/data/d_com_sum_h2A.csv"))
+data.table::fwrite(d_com_ctrl_h2D_sum, paste0(path, "getH2_newTestCross/data/d_com_sum_h2D.csv"))
+data.table::fwrite(d_com_ctrl_h2AA_sum, paste0(path, "getH2_newTestCross/data/d_com_sum_h2AA.csv"))
+data.table::fwrite(d_com_ctrl_sum, paste0(path, "getH2_newTestCross/data/d_com_sum.csv"))
