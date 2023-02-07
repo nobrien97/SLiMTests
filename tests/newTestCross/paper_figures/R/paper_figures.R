8689bdedfb955464df26919578f6f0d3d8a57cb4
nobrien97 <nicholasobrien97@gmail.com>
2023-02-07 16:55:48 +1000

    Started paper figures/stats

---
+++ tests/newTestCross/paper_figures/R/paper_figures.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+library(grid)
+library(gridExtra)
+library(latex2exp)
+library(cowplot)
+
+se <- function(x, na.rm = F) {
+  if (na.rm)
+    x <- x[!is.na(x)]
+  
+  return(sd(x)/sqrt(length(x)))
+}
+
+# Filepath
+path_add <- "/mnt/d/SLiMTests/tests/newTestCross/additive/getH2_newTestCross/data/"
+path_net <- "/mnt/d/SLiMTests/tests/newTestCross/moreReps/getH2_newTestCross/data/"
+
+# Colour palette
+cc_ibm <- c("#648fff", "#785ef0", "#dc267f", "#fe6100", "#ffb000", "#000000")
+
+
+# Pheno time add/net
+pheno_time_add <- readRDS(paste0(path_add, "pheno_time_add.RDS"))
+pheno_time_net <- readRDS(paste0(path_net, "pheno_time_unfixed.RDS"))
+
+pheno_time_leg <- get_legend(pheno_time_add)
+
+pheno_time <- plot_grid(
+  pheno_time_add + theme(legend.position = "none", panel.spacing = unit(1, "lines")),
+  pheno_time_net + theme(legend.position = "none", panel.spacing = unit(1, "lines")),
+  nrow = 2, labels = "AUTO"
+)
+
+pheno_time <- plot_grid(pheno_time, pheno_time_leg, nrow = 2, rel_heights = c(3, .4))
+pheno_time
+
+ggsave("pheno_time.png", pheno_time, width = 8, height = 10, bg = "white")
+
+
+# GAM model
+pred_gam <- readRDS("./pred_gam.RDS")
+pred_gam %>% mutate(gen = gen - 50000) %>%
+  ggplot(aes(gen, fit, colour = model)) +
+  facet_grid(nloci~sigma) +
+  scale_fill_manual(values = cc_ibm[c(1, 3)], guide = "none") +
+  scale_color_manual(values = cc_ibm[c(1, 3)]) +
+  geom_line() +
+  #geom_point(data = d_qg_sbst %>% filter(phenomean < 3), aes(y = phenomean), size = 0.1) +
+  geom_ribbon(aes(ymin = (fit - se.fit), ymax = (fit + se.fit), 
+                  fill = model), color = NA, alpha = 0.2) +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Number of QTLs", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Mutational effect variance", 
+                                         breaks = NULL, labels = NULL)) +
+  labs(x = "Generations after optimum shift", y = TeX("$E\\[\\mu_{\\bar{z}}\\]$"), color = "Model") +
+  theme_bw() +
+  theme(text=element_text(size=16), legend.position = "bottom")
+
+ggsave("pheno_GAM.png", width = 8, height = 6, bg = "white")
+
---
+++ tests/newTestCross/paper_figures/R/paper_stats.R
@@ -0,0 +1,74 @@
+library(tidyverse)
+library(mgcv)
+library(ggeffects)
+library(tidymv)
+library(latex2exp)
+
+# First need to combine additive/network data with a model type parameter
+path_add <- "/mnt/d/SLiMTests/tests/newTestCross/additive/"
+path_net <- "/mnt/d/SLiMTests/tests/newTestCross/getH2_newTestCross/data/"
+
+d_qg_add <- read_csv(paste0(path_add, "slim_qg.csv"), col_names = F)
+names(d_qg_add) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean", 
+                 "phenovar", "dist", "w", "deltaPheno", "deltaw")
+d_qg_add$model <- "Additive"
+
+combos_add <- read_delim("/mnt/c/GitHub/SLiMTests/tests/newTestCross/additive/R/combos.csv", delim = " ", col_names = F)
+names(combos_add) <- c("nloci", "locisigma")
+d_qg_add %>% mutate(nloci = combos_add$nloci[.$modelindex],
+                sigma = combos_add$locisigma[.$modelindex]) -> d_qg_add
+
+
+d_qg_net <- read_csv(paste0(path_net, "slim_qg_total.csv"), col_names = F)
+names(d_qg_net) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean", 
+                 "phenovar", "dist", "w", "deltaPheno", "deltaw", "aZ", "bZ", "KZ", "KXZ")
+d_qg_net$model <- "NAR"
+
+combos_net <- read_delim("/mnt/c/GitHub/SLiMTests/tests/newTestCross/R/combos.csv", delim = " ", col_names = F)
+names(combos_net) <- c("model", "nloci", "locisigma")
+d_qg_net %>% mutate(fixedEffect = combos_net$model[.$modelindex],
+                nloci = combos_net$nloci[.$modelindex],
+                sigma = combos_net$locisigma[.$modelindex]) -> d_qg_net
+
+# Recode fixedEffect to mol trait name
+d_qg_net %>% mutate(fixedEffect = recode_factor(fixedEffect, `-1`="None", `0`="\u03B1", `1`="\u03B2", `2`="KZ", `3`="KXZ")) -> d_h2
+
+d_qg_net$nloci <- d_qg_net$nloci + 2
+
+d_qg <- bind_rows(d_qg_add, d_qg_net)
+rm(d_qg_add, d_qg_net)
+
+d_qg <- d_qg %>%
+  mutate(seed = as_factor(seed),
+         modelindex = as_factor(modelindex),
+         model = as_factor(model),
+         nloci = as_factor(nloci),
+         sigma = as_factor(sigma),
+         fixedEffect = as_factor(fixedEffect))
+
+# Using GAMs to fit out phenotype-time model
+# u_z ~ gen + nloci + locisigma + c
+d_qg_sbst <- d_qg %>% filter(fixedEffect == -1 | is.na(fixedEffect),
+                             gen >= 50000,
+                             phenomean < 10)
+
+mod_gam <- gam(phenomean ~ s(gen, k = 12) + model * nloci * sigma, 
+               data = d_qg_sbst, family = scat(link = "identity"), method = "REML")
+summary(mod_gam)
+plot(mod_gam)
+gam.check(mod_gam)
+concurvity(mod_gam)
+
+# Test against a linear model
+mod_lm <- gam(phenomean ~ gen + model * nloci * sigma, data = d_qg_sbst, method = "REML")
+summary(mod_lm)
+
+# GAM fits better
+AIC(mod_gam); AIC(mod_lm)
+summary(mod_gam)$sp.criterion; summary(mod_lm)$sp.criterion
+
+plot(ggpredict(mod_gam), facets = T)
+
+pred_gam <- predict_gam(mod_gam)
+saveRDS(pred_gam, "/mnt/c/GitHub/SLiMTests/tests/newTestCross/paper_figures/R/pred_gam.RDS")
+saveRDS(mod_gam, "/mnt/c/GitHub/SLiMTests/tests/newTestCross/paper_figures/R/mod_gam.RDS")
