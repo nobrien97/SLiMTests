3bebcc12d2ed6a2f6162326d2d74584d23aaef2c
nobrien97 <nicholasobrien97@gmail.com>
2023-04-13 18:13:44 +1000

    more landscape plots

--- tests/equalK_sweep/equalK_ODE/R/phenoLandscape.R
+++ tests/equalK_sweep/equalK_ODE/R/phenoLandscape.R
@@ -73,110 +73,58 @@ library(ggarrow)
 d_ode_phasemeasures$gen_width <- rescale(d_ode_phasemeasures$gen, to = c(0.001, 1))
 
 sampled_id <- sample(d_ode_phasemeasures$id, 1)
+# Choose one with a huge KZ
+sampled_id <- d_ode_phasemeasures[order(d_ode_phasemeasures$KZ, decreasing = T),]$id[1]
 
 cc <- paletteer_c("ggthemes::Orange-Blue Diverging", 3)
 cc <- c(cc[1], cc[3], cc[2])
+cc2 <- paletteer_c("grDevices::Emrld", 50, -1)
 
 d_ode_phasemeasures2 <- d_ode_phasemeasures
+d_ode_phasemeasures <- d_ode_phasemeasures2
 d_ode_phasemeasures <- d_ode_phasemeasures %>% filter(id %in% sampled_id)
 
-aZbZScatter <- ggplot(d_ode_phasemeasures, aes(x = aZ, y = bZ, colour = pheno_rescaled)) +
-  geom_point() +
-  geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
-               mapping = aes(x = lag(aZ), y = lag(bZ), xend = aZ, yend = bZ, 
-                             group = id, linewidth_head = gen_width, 
-                             linewidth_fins = gen_width * 0.8), color = "black", 
-               arrow_head = arrow_head_line()) +
-  scale_linewidth(guide = "none") +
-  scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
-                       limits = c(0, 4),
-                       labels = c(seq(0, 2, 1), "3"),
-                       breaks = seq(0, 3, 1)
-  ) +
-  labs(x = TeX("$\\alpha_Z$"), y = TeX("$\\beta_Z$"), colour = "Phenotype (Z)") +
-  theme_bw() + 
-  theme(legend.position = "bottom") +
-  guides(colour=guide_colourbar(barwidth=20))
+# plot qg
+d_qg$id <- as.factor(paste(d_qg$seed, d_qg$modelindex, sep = "_"))
+d_qg$pheno_rescaled <- ifelse(d_qg$phenomean > 3, 4, d_qg$phenomean)
+d_qg$gen_width <- rescale(d_qg$gen, to = c(0.001, 1))
 
-aZKZScatter <- ggplot(d_ode_phasemeasures, aes(x = aZ, y = KZ, colour = pheno_rescaled)) +
-  geom_point() +
-  geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
-               mapping = aes(x = lag(aZ), y = lag(KZ), xend = aZ, yend = KZ, 
-                             group = id, linewidth_head = gen_width, 
-                             linewidth_fins = gen_width * 0.8), color = "black", 
-               arrow_head = arrow_head_line()) +
-  scale_linewidth(guide = "none") +
-  scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
-                         limits = c(0, 4),
-                         labels = c(seq(0, 2, 1), "3"),
-                         breaks = seq(0, 3, 1)
-                         ) +
-  labs(x = TeX("$\\alpha_Z$"), y = TeX("$K_Z$"), colour = "Phenotype (Z)") +
-  theme_bw()
+d_ode_phasemeasures <- d_qg %>% filter(id %in% sampled_id)
 
-aZKXZScatter <- ggplot(d_ode_phasemeasures, aes(x = aZ, y = KXZ, colour = pheno_rescaled)) +
-  geom_point() +
-  geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
-               mapping = aes(x = lag(aZ), y = lag(KXZ), xend = aZ, yend = KXZ, 
-                             group = id, linewidth_head = gen_width, 
-                             linewidth_fins = gen_width * 0.8), color = "black", 
-               arrow_head = arrow_head_line()) +
-  scale_linewidth(guide = "none") +
-  scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
-                         limits = c(0, 4),
-                         labels = c(seq(0, 2, 1), "3"),
-                         breaks = seq(0, 3, 1)
-  ) +
-  labs(x = TeX("$\\alpha_Z$"), y = TeX("$K_{XZ}$"), colour = "Phenotype (Z)") +
-  theme_bw()
+library(ggnewscale)
 
-bZKZScatter <- ggplot(d_ode_phasemeasures, aes(x = bZ, y = KZ, colour = pheno_rescaled)) +
-  geom_point() +
-  geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
-               mapping = aes(x = lag(bZ), y = lag(KZ), xend = bZ, yend = KZ, 
-                             group = id, linewidth_head = gen_width, 
-                             linewidth_fins = gen_width * 0.8), color = "black", 
-               arrow_head = arrow_head_line()) + 
-  scale_linewidth(guide = "none") +
-  scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
-                         limits = c(0, 4),
-                         labels = c(seq(0, 2, 1), "3"),
-                         breaks = seq(0, 3, 1)
-  ) +
-  labs(x = TeX("$\\beta_Z$"), y = TeX("$K_Z$"), colour = "Phenotype (Z)") +
-  theme_bw()
+plotPairwiseScatter <- function(x, y, labels) {
+  ggplot(d_ode_phasemeasures, aes(x = .data[[x]], y = .data[[y]], colour = pheno_rescaled)) +
+    geom_point() +
+    scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
+                           limits = c(0, 4),
+                           labels = c(seq(0, 2, 1), "3"),
+                           breaks = seq(0, 3, 1)
+    ) +
+    labs(colour = "Phenotype (Z)") +
+    
+    new_scale_colour() +
+    geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
+                       mapping = aes(x = lag(.data[[x]]), y = lag(.data[[y]]), 
+                                     xend = .data[[x]], yend = .data[[y]], 
+                                     group = id, linewidth_head = gen_width, 
+                                     linewidth_fins = gen_width * 0.8,
+                                     colour = gen_width), 
+                       arrow_head = arrow_head_line()) +
+    scale_colour_gradientn(colors = cc2, labels = c(0, 0.25*2500, 0.5*2500, 0.75*2500, 2500)) +
+    scale_linewidth(guide = "none") +
+    labs(x = labels[1], y = labels[2], colour = "Generation") +
+    theme_bw() + 
+    theme(legend.position = "bottom") +
+    guides(colour=guide_colourbar(barwidth=20))
+}
 
-bZKXZScatter <- ggplot(d_ode_phasemeasures, aes(x = bZ, y = KXZ, colour = pheno_rescaled)) +
-  geom_point() +
-  geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
-               mapping = aes(x = lag(bZ), y = lag(KXZ), xend = bZ, yend = KXZ, 
-                             group = id, linewidth_head = gen_width, 
-                             linewidth_fins = gen_width * 0.8), color = "black", 
-               arrow_head = arrow_head_line()) + 
-  scale_linewidth(guide = "none") +
-  scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
-                         limits = c(0, 4),
-                         labels = c(seq(0, 2, 1), "3"),
-                         breaks = seq(0, 3, 1)
-  ) +
-  labs(x = TeX("$\\beta_Z$"), y = TeX("$K_{XZ}$"), colour = "Phenotype (Z)") +
-  theme_bw()
-
-KZKXZScatter <- ggplot(d_ode_phasemeasures, aes(x = KZ, y = KXZ, colour = pheno_rescaled)) +
-  geom_point() +
-  geom_arrow_segment(data = d_ode_phasemeasures %>% filter(id %in% sampled_id), 
-               mapping = aes(x = lag(KZ), y = lag(KXZ), xend = KZ, yend = KXZ, 
-                             group = id, linewidth_head = gen_width, 
-                             linewidth_fins = gen_width * 0.8), color = "black", 
-               arrow_head = arrow_head_line()) +
-  scale_linewidth(guide = "none") +
-  scale_colour_gradientn(colors = cc, values = rescale(c(0, 2, 4)),
-                         limits = c(0, 4),
-                         labels = c(seq(0, 2, 1), "3"),
-                         breaks = seq(0, 3, 1)
-  ) +
-  labs(x = TeX("$K_Z$"), y = TeX("$K_{XZ}$"), colour = "Phenotype (Z)") +
-  theme_bw()
+aZbZScatter <- plotPairwiseScatter("aZ", "bZ", c(TeX("$\\alpha_Z$"), TeX("$\\beta_Z$")))
+aZKZScatter <- plotPairwiseScatter("aZ", "KZ", c(TeX("$\\alpha_Z$"), TeX("$K_Z$")))
+aZKXZScatter <- plotPairwiseScatter("aZ", "KXZ", c(TeX("$\\alpha_Z$"), TeX("$K_{XZ}$")))
+bZKZScatter <- plotPairwiseScatter("bZ", "KZ", c(TeX("$\\beta_Z$"), TeX("$K_Z$")))
+bZKXZScatter <- plotPairwiseScatter("bZ", "KXZ", c(TeX("$\\beta_Z$"), TeX("$K_{XZ}$")))
+KZKXZScatter <- plotPairwiseScatter("KZ", "KXZ", c(TeX("$K_Z$"), TeX("$K_{XZ}$")))
 
 plot_list <- list(ggally_densityDiag(d_ode_phasemeasures, mapping = aes(x = aZ)), 
                   ggally_cor(d_ode_phasemeasures, mapping = aes(x = aZ, y = bZ)),
@@ -206,7 +154,7 @@ ggmatrix(plot_list, nrow = 4, ncol = 4, xAxisLabels = xlabs, yAxisLabels = xlabs
          legend = grab_legend(plot_list[[5]])) + theme_bw() + 
   theme(legend.position = "bottom") -> pair_mat
 
-ggsave("molTrait_landscape_singleWalk.png", pair_mat, width = 11, height = 8)
+ggsave("molTrait_landscape_singleWalk2.png", pair_mat, width = 11, height = 8)
 
 ggpairs(d_ode_phasemeasures, columns = c(6:9))
 
@@ -239,4 +187,33 @@ d_qg %>% mutate(KZ_bigger = (2 * KZ > aZ) & (2 * KZ > bZ), (2 * KZ > KXZ)) %>%
 
 plot(KZ_bigger$gen, KZ_bigger$`mean(KZ_bigger)`, type = "b")
 
+d_pheno <- d_ode_phasemeasures %>%
+  pivot_longer(cols = c(phenomean, aZ, bZ, KZ, KXZ), names_to = "trait", values_to = "value")
+
+library(cowplot)
+
+molTrait_names <- c(TeX("$\\alpha_Z$"), TeX("$\\beta_Z$"), 
+                    TeX("$K_Z$"), TeX("$K_{XZ}$"), TeX("Z"))
+
+ggplot(d_pheno %>% filter(gen > 49000, id %in% sampled_id) %>% mutate(gen = gen - 50000),
+       aes(x = gen, y = value, colour = trait)) +
+  geom_line() +
+  scale_colour_paletteer_d("ggsci::nrc_npg", 1) +
+  geom_point() +
+  labs(x = "Generations post-optimum shift", y = "Mean trait/\ncomponent value",
+       colour = "Trait/component") +
+  theme_bw() + 
+  theme(text = element_text(size = 16), legend.position = "bottom") -> singlewalk2_pheno
+
+plot_grid(singlewalk2_pheno + coord_cartesian(ylim = c(0, 3)) + theme(legend.position = "none"), 
+          singlewalk2_pheno + 
+            coord_cartesian(ylim = c(min(d_ode_phasemeasures$KZ), 
+                                     max(d_ode_phasemeasures$KZ))) + 
+            guides(colour = "none"),
+          nrow = 2) -> pheno_grid
+
+plot_grid(pheno_grid, get_legend(singlewalk2_pheno), ncol = 1, rel_heights = c(1, 0.1))
+
+
+ggsave("molTrait_phenowalk2.png", width = 7, height = 6, bg = "white")
 # Evidence for KXZ neutral evolution between 0 - 1
