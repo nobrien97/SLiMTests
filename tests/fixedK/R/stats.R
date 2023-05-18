8f43b100ebba1fd452d9ab37068c3de7ca32ad16
nobrien97 <nicholasobrien97@gmail.com>
2023-05-18 18:00:35 +1000

    plot shape par estimate

--- tests/fixedK/R/stats.R
+++ tests/fixedK/R/stats.R
@@ -10,21 +10,45 @@ plot(logspline(x),add=T)
 
 # Set seed for reproducibility - 6673185 
 seed <- sample(0:.Machine$integer.max, 1)
-set.seed(seed)
+#set.seed(seed)
+set.seed(6673185)
 
 # Fitness effects
 fit_add <- fevd(d_fix_add$avFit, method = "Lmoments")
 plot(fit_add)
 # bootstrap to find CIs
-ci(fit_add, R = 1000, type = "parameter")
+addci <- ci(fit_add, R = 1000, type = "parameter")
 
 fit_aZ <- fevd(d_fix_aZ$avFit, method = "Lmoments")
 plot(fit_aZ)
-ci(fit_aZ, R = 1000, type = "parameter")
+aZci <- ci(fit_aZ, R = 1000, type = "parameter")
 
 fit_bZ <- fevd(d_fix_bZ$avFit, method = "Lmoments")
 plot(fit_bZ)
-ci(fit_bZ, R = 1000, type = "parameter")
+bZci <- ci(fit_bZ, R = 1000, type = "parameter")
+
+attr(addci, "class") <- NULL
+attr(aZci, "class") <- NULL
+attr(bZci, "class") <- NULL
+
+shape_plt <- data.frame(effectType = c(TeX("$Additive$", output = "character"), 
+                                       TeX("$\\alpha_Z$", output = "character"), 
+                                       TeX("$\\beta_Z$", output = "character")),
+                        estimate = c(addci[3,2], aZci[3,2], bZci[3,2]),
+                        minCI = c(addci[3,1], aZci[3,1], bZci[3,1]),
+                        maxCI = c(addci[3,3], aZci[3,3], bZci[3,3]))
+
+# Figure S1: bar plot of shape parameters
+ggplot(shape_plt, aes(x = effectType, y = estimate)) +
+  geom_point(size = 2) +
+  geom_errorbar(aes(ymin = minCI, ymax = maxCI), width = 0.4) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_y_continuous(limits = c(-0.25, 0.25)) +
+  scale_x_discrete(labels = parse(text = shape_plt$effectType)) +
+  labs(x = "Allelic effect type", y = TeX("Estimated shape parameter ($\\xi$)")) +
+  theme_bw() +
+  theme(text = element_text(size = 16))
+ggsave("GEVshape_parameter_fitness.png", device = png)
 
 # Phenotype effects
 fit_add <- fevd(d_fix_add$value, method = "Lmoments")
