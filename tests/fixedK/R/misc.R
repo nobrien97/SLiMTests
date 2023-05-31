3dadfb9c56b22e752885ecf974b6059d53ced685
nobrien97 <nicholasobrien97@gmail.com>
2023-05-31 17:15:23 +1000

    test to look at fitness calculation for addition of one large effect allele

--- tests/fixedK/R/misc.R
+++ tests/fixedK/R/misc.R
@@ -59,3 +59,24 @@ ggplot(d_com_adapted %>% filter(modelindex == 1, seed == 1448101263,
   labs(x = "Generations post-optimum shift", y = "Allele frequency", colour = "Mutation") +
   theme(text = element_text(size = 16))
 
+# see what's going on with fitness effect estimation for NAR models
+# 
+write.table(d_seg_ranked %>% filter(seed == 2016338465, gen == 59700, mutType == 3) %>% ungroup() %>% 
+              dplyr::select(aZ, bZ, KZ, KXZ), 
+            "d_grid_aZ.csv", sep = ",", col.names = F, row.names = F)
+d_dat_aZ_absence <- d_seg_ranked %>% filter(seed == 2016338465, gen == 59700, mutType == 3)
+d_dat_aZ_absence$aZ <- exp(log(d_dat_aZ_absence$aZ) - d_dat_aZ_absence$value)
+
+write.table(d_dat_aZ_absence %>% ungroup() %>% dplyr::select(aZ, bZ, KZ, KXZ), 
+            "d_grid_aZ_absence.csv", sep = ",", col.names = F, row.names = F)
+
+d_popfx_aZ_absence <- runLandscaper("d_grid_aZ_absence.csv", "data_popfx_aZ_absence.csv", 0.05, 2, 8)
+d_popfx_aZ <- runLandscaper("d_grid_aZ.csv", "data_popfx_aZ.csv", 0.05, 2, 8)
+d_popfx_aZ$fitness - d_popfx_aZ_absence$fitness
+
+
+d_seg_ranked %>%
+  filter(rank > 0, gen == 59700, seed == 2016338465) %>%
+  mutate(curEffect = abs(.data[["avFX"]])) %>%
+  summarise(segEffectSum = sum(curEffect))
+
