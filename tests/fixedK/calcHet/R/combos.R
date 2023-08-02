c0b9cc8d19c11661e1d14573e5d6c3b5ab3281bc
nobrien97 <nicholasobrien97@gmail.com>
2023-08-02 19:24:09 +1000

    combos for het

---
+++ tests/fixedK/calcHet/R/combos.R
@@ -0,0 +1,28 @@
+# Create cmds.txt and combo file
+path <- "/mnt/c/GitHub/SLiMTests/tests/fixedK/calcHet/R/"
+setwd(path)
+# Generate cmds.txt
+singleRunBashName <- "./calcHetSR.sh"
+
+library(tidyverse)
+
+# get list of seeds from fixedK, moreReps, moreReps2
+moreReps_seeds <- read.csv("/mnt/c/GitHub/SLiMTests/tests/fixedK/moreReps/R/fixedK_seeds.csv", header = F)
+moreReps2_seeds <- read.csv("/mnt/c/GitHub/SLiMTests/tests/fixedK/moreReps2/R/fixedK_seeds.csv", header = F)
+
+seeds <- rbind(moreReps_seeds, moreReps2_seeds)
+
+modelindex <- 1:2
+seeds <- data.frame(seed = seeds[rep(seq_len(nrow(seeds)), each = 2), ],
+                    modelindex = 1:2)
+head(seeds)
+
+seeds$file <- paste0("/g/data/ht96/nb9894/fixedK/pop_states/slim_popstate", seeds$seed, "_", seeds$modelindex, ".bin")
+
+
+cmds <- data.frame(sr = singleRunBashName,
+                   modelindex = seeds$modelindex,
+                   seed = seeds$seed,
+                   file = seeds$file)
+
+write.table(cmds, "/mnt/c/GitHub/SLiMTests/tests/fixedK/calcHet/PBS/cmds.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
