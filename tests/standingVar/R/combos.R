03c55f5f6910c082c85d58fc9221051b350875fa
nobrien97 <nicholasobrien97@gmail.com>
2023-11-08 17:46:21 +1000

    chapter 2: generate hypercube

--- tests/standingVar/R/combos.R
+++ tests/standingVar/R/combos.R
@@ -1,25 +1,59 @@
 # Create cmds.txt and combo file
-path <- "/mnt/c/GitHub/SLiMTests/tests/fixedK/moreReps/R/"
+path <- "/mnt/c/GitHub/SLiMTests/tests/standingVar/R/"
 setwd(path)
 # Generate cmds.txt
-singleRunBashName <- "./fixedKSR.sh"
+singleRunBashName <- "./standingVarSR.sh"
 
-seed_path <- "/mnt/c/GitHub/SLiMTests/tests/fixedK/moreReps2/R/fixedK_seeds.csv"
-system(paste0("SeedGenerator -n 1440 -t -d ", seed_path))
+seed_path <- "/mnt/c/GitHub/SLiMTests/tests/standingVar/R/standingVar_seeds.csv"
+system(paste0("SeedGenerator -n 100 -t -d ", seed_path))
 
 library(tidyverse)
+library(DoE.wrapper)
+library(DiceDesign)
 
-seed <- sample(1:.Machine$integer.max, 1)
-# sampled seed: 805432144
-set.seed(seed)
-combos <- c(2, 4)
+iter <- 0
+repeat {
+  if (iter >= 100) {
+    errorCondition("Unable to find hypercube with max correlation <0.001 in 100 iterations")
+    break 
+  }
+  seed <- sample(1:.Machine$integer.max, 1)
+  set.seed(seed)
+  lhc <- lhs.design(
+    nruns = 256,
+    nfactors = 3,
+    type = "maximin",
+    factor.names = list(
+      "nloci" = c(1, 1000),
+      "tau"   = c(0.01, 1.5),
+      "rwide" = c(0, 0.5)
+    ),
+    seed = seed
+  )
+  iter = iter + 1
+  maxCor <- max(abs(cor(lhc)[upper.tri(cor(lhc))]))
+  if (maxCor < 0.001)
+    break
+} 
 
-write_csv(data.frame(nloci = combos), "combos.csv", col_names = F)
+# nloci to integer
+lhc$nloci <- ceiling(lhc$nloci)
 
-seeds <- 1:1440
+
+# final seed: 324263921
+
+# Calculate l2-star discrepancy: values close to 0 indicate good spread, 1 is bad
+# measures overall uniformity
+discrepancyCriteria(lhc, "L2star")
+# 0.006626378
+
+write_csv(lhc, "combos.csv", col_names = F)
+
+seeds <- 1:100
 
 cmds <- data.frame(sr = singleRunBashName,
-                   nloci = rep(1:length(combos), each = length(seeds)),
-                   seed = rep(1:length(seeds), times = length(combos)))
+                   model = rep(1:nrow(lhc), each = length(seeds)),
+                   seed = rep(1:length(seeds), times = nrow(lhc)))
 
-write.table(cmds, "/mnt/c/GitHub/SLiMTests/tests/fixedK/moreReps/PBS/cmds.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(cmds, "/mnt/c/GitHub/SLiMTests/tests/standingVar/PBS/cmds.txt", 
+            sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
