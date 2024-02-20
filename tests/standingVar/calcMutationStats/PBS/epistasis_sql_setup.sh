8bc214b3b043657f45534508bd9626a2fcb2218d
nobrien97 <nicholasobrien97@gmail.com>
2024-02-20 17:48:44 +1000

    setup sql database for pairwise epistasis

---
+++ tests/standingVar/calcMutationStats/PBS/epistasis_sql_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#PBS -P ht96
+#PBS -q hugemem
+#PBS -l walltime=48:00:00
+#PBS -l ncpus=4
+#PBS -l mem=1470GB
+#PBS -l iointensive=1
+#PBS -l storage=scratch/ht96+gdata/ht96
+
+# Purpose: Sets up a sql database for mutation data
+
+# Path to updated version of sqlite
+SQLITE3=~/Tools/sqlite/sqlite3
+SAVEDIR=/g/data/ht96/nb9894/standingVar/calcMutationStats
+RUNDIR=/iointensive
+
+cd $PBS_O_WORKDIR
+
+# Loads data into data frame and attaches indices
+cat ./load_data.sql | $SQLITE3 $RUNDIR/epistasis.db 2>/dev/null
+cat ./add_indices.sql | $SQLITE3 $RUNDIR/epistasis.db 2>/dev/null
+
+mv $RUNDIR/epistasis.db* $SAVEDIR
---
+++ tests/standingVar/calcMutationStats/PBS/load_data.sql
@@ -0,0 +1,41 @@
+PRAGMA journal_mode = OFF;
+PRAGMA synchronous = OFF;
+PRAGMA cache_size = 7500000;
+
+CREATE TABLE tab_epistasis(
+    gen             INTEGER,
+    seed            INTEGER,
+    modelindex      INTEGER,
+    mutType_ab      TEXT,
+    wa              REAL,
+    wb              REAL,
+    wab             REAL,
+    Pwt             REAL,
+    Pa              REAL,
+    Pb              REAL,
+    Pab             REAL,
+    ew              REAL,
+    ep              REAL
+);
+
+.mode csv
+.import /g/data/ht96/nb9894/standingVar/calcMutationStats/d_epistasis.csv tab_epistasis
+
+CREATE TABLE tab_epistasis_freq(
+    gen             INTEGER,
+    seed            INTEGER,
+    modelindex      INTEGER,
+    mutType_ab      TEXT,
+    wa              REAL,
+    wb              REAL,
+    wab             REAL,
+    Pwt             REAL,
+    Pa              REAL,
+    Pb              REAL,
+    Pab             REAL,
+    ew              REAL,
+    ep              REAL
+);
+
+.mode csv
+.import /g/data/ht96/nb9894/standingVar/calcMutationStats/d_epistasis_freqweight.csv tab_epistasis_freq
