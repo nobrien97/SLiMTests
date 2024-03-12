d435f4508ec2bf41009dae8ce90ac4dd7e19d44c
nobrien97 <nicholasobrien97@gmail.com>
2024-03-12 18:21:44 +1000

    calculate LD during adaptation

---
+++ tests/standingVar/calcLD/PBS/calcLDSR.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -l
+
+cd $PBS_JOBFS
+SECONDS=0
+
+# Rename the first and second arguments passed to this single shot script for clarity 
+MODELINDEX=$1
+SEED=$2
+FILENAME=${MODELINDEX}_${SEED}
+BASENAME=standingVar
+JOBNAME=calcLD
+BASEDIR=$HOME/tests/$BASENAME
+TESTDIR=$BASEDIR/$JOBNAME
+
+echo "Beginning run modelindex = $MODELINDEX, seed = $SEED at $(date)"
+
+if [ -f $TESTDIR/done/${FILENAME} ]; then
+    echo "$FILENAME already done! Moving to next simulation."
+    exit 0
+fi
+
+# Get the correct modelindex from the file: put into array
+MODEL_FILE=$BASEDIR/R/combos.csv
+MODEL_NUM=($(awk "NR==$MODELINDEX" $MODEL_FILE))
+SEED_FILE=$TESTDIR/R/${BASENAME}_seeds.csv
+SEED_NUM=($(awk "NR==$SEED" $SEED_FILE))
+
+# Extract necessary files from /g/data
+SEED=1251567054
+MODELINDEX=29
+awk -F, '$1 == 29 && $2 == 1251567054' slim_pos.csv > slim_pos${SEED}_${MODELINDEX}.csv
+
+# Everything I can find says this SHOULD WORK but it DOESN'T so we're going to do an R script instead
+# I hate this
+#awk -F',' -v a="$SEED" -v b="$MODELINDEX" '$1 == a && $2 == b' slim_pos.csv > slim_pos${SEED}_${MODELINDEX}.csv
+
+
+
+# Run the model
+echo "Running modelindex = $MODELINDEX, seed = $SEED...\n"
+# If we have a K model, we need to disable molTraitFix by setting it to -1
+if [[ "${MODEL_NUM[3]}" == "'K'" ]]
+then
+    $HOME/SLiM/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d inputSeed=${SEED_NUM} -d inputModel=$MODELINDEX -d molTraitFix=-1 -d molTraitProps="c(0.25, 0.25, 0.25, 0.25)" -d nloci=${MODEL_NUM[0]} -d locisigma=${MODEL_NUM[1]} -d rwide=${MODEL_NUM[2]} -d modelType="'ODE'" $TESTDIR/slim/calcLD.slim
+else
+    $HOME/SLiM/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d inputSeed=${SEED_NUM} -d inputModel=$MODELINDEX -d molTraitFix="c(2,3)" -d molTraitProps="c(0.5, 0.5, 0.0, 0.0)" -d nloci=${MODEL_NUM[0]} -d locisigma=${MODEL_NUM[1]} -d rwide=${MODEL_NUM[2]} -d modelType="${MODEL_NUM[3]}" $TESTDIR/slim/calcLD.slim
+fi
+
+DURATION=$SECONDS
+echo "Run modelindex = $MODELINDEX, seed = $SEED finished at $(date)!"
+echo "$(($DURATION / 3600)) hours, $((($DURATION / 60) % 60)) minutes, and $(($DURATION % 60)) seconds elapsed."
+
+# Create file to show what we've already done if we get interrupted
+touch $TESTDIR/done/${FILENAME}
