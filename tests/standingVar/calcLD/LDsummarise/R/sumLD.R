a29179ac847e1872364efe742785ae974d318c68
nobrien97 <nicholasobrien97@gmail.com>
2024-03-13 19:46:54 +1000

    LD summary statistics calculation

--- tests/standingVar/calcLD/LDsummarise/R/sumLD.R
+++ tests/standingVar/calcLD/LDsummarise/R/sumLD.R
@@ -11,17 +11,35 @@ library(data.table)
 args <- commandArgs(trailingOnly = T)
 model <- as.numeric(args[1])
 
-R_PATH <- "~/tests/standingVar/calcMutationStats/R/"
-source(paste0(R_PATH, "helperFunctionsAndSetup.R"))
 GDATA_PATH <- "/g/data/ht96/nb9894/standingVar/calcLD/"
 
+FILE_NAME <- paste0("/scratch/ht96/nb9894/standingVar/calcLD/sumLD", model, ".csv")
+
 # TODO: check model off by one error
-ld_pos <- scan(paste0("slim_ld_pos.csv"), skip = model, sep = ",")
+# ld_pos <- scan(paste0("slim_ld_pos.csv"), skip = model, sep = ",")
 
-model_info <- ld_pos[1:3]
-ld_pos <- ld_pos[-(1:3)]
 
-ld_val <- scan(paste0("slim_ld_val.csv"), skip = model, sep = ",")
+ld_val <- scan(paste0(GDATA_PATH, "slim_ld_val.csv"), skip = model, sep = ",")
+model_info <- ld_val[1:3]
+ld_val <- ld_val[-(1:3)]
 
-# Expand to matrix
+# Maximum number of non-zero element
+max_elements <- 1024*1024
 
+# Calculate means (don't need pos?)
+# means/sd with and without the zeroes added (just change denominator to include zeroes)
+result <- tibble() %>%
+    mutate(
+        gen = model_info[1],
+        seed = model_info[2],
+        modelindex = model_info[3],
+        meanD = mean(ld_val),
+        sdD = sd(ld_val),
+        nD = length(ld_val),
+        propNonZeroD = length(ld_val)/max_elements,
+        meanDZeros = sum(ld_val) / (max_elements),
+        sdDZeros = sqrt( ( sum((ld_val - meanDZeros)^2) ) / max_elements )
+    )
+
+# Write output
+write.table(result, FILE_NAME, row.names = F, col.names = F)
