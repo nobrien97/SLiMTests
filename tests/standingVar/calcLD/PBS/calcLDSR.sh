c07d0501841feda3f98d13cd188fa4ec4f92dce0
nobrien97 <nicholasobrien97@gmail.com>
2024-03-13 18:17:12 +1000

    fixed malformed input to slim

--- tests/standingVar/calcLD/PBS/calcLDSR.sh
+++ tests/standingVar/calcLD/PBS/calcLDSR.sh
@@ -33,12 +33,16 @@ awk -F',' -v a="$MODELINDEX" -v b="$SEED" '$1 == b && $2 == a' $DATAPATH/slim_op
 
 # Run the model
 echo "Running modelindex = $MODELINDEX, seed = $SEED...\n"
+
+# Convert PBS_JOBFS to a string for SLiM to read
+JOBFS_STR=\'$PBS_JOBFS\'
+
 # If we have a K model, we need to disable molTraitFix by setting it to -1
 if [[ "${MODEL_NUM[3]}" == "'K'" ]]
 then
-    $HOME/SLiM/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d inputSeed=${SEED_NUM} -d inputModel=$MODELINDEX -d posPath="${PBS_JOBFS}" -d molTraitFix=-1 -d molTraitProps="c(0.25, 0.25, 0.25, 0.25)" -d nloci=${MODEL_NUM[0]} -d locisigma=${MODEL_NUM[1]} -d rwide=${MODEL_NUM[2]} -d modelType="'ODE'" $TESTDIR/slim/calcLD.slim
+    $HOME/SLiM/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d inputSeed=${SEED_NUM} -d inputModel=$MODELINDEX -d posPath="${JOBFS_STR}" -d molTraitFix=-1 -d molTraitProps="c(0.25, 0.25, 0.25, 0.25)" -d nloci=${MODEL_NUM[0]} -d locisigma=${MODEL_NUM[1]} -d rwide=${MODEL_NUM[2]} -d modelType="'ODE'" $TESTDIR/slim/calcLD.slim
 else
-    $HOME/SLiM/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d inputSeed=${SEED_NUM} -d inputModel=$MODELINDEX -d posPath="${PBS_JOBFS}" -d molTraitFix="c(2,3)" -d molTraitProps="c(0.5, 0.5, 0.0, 0.0)" -d nloci=${MODEL_NUM[0]} -d locisigma=${MODEL_NUM[1]} -d rwide=${MODEL_NUM[2]} -d modelType="${MODEL_NUM[3]}" $TESTDIR/slim/calcLD.slim
+    $HOME/SLiM/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d inputSeed=${SEED_NUM} -d inputModel=$MODELINDEX -d posPath="${JOBFS_STR}" -d molTraitFix="c(2,3)" -d molTraitProps="c(0.5, 0.5, 0.0, 0.0)" -d nloci=${MODEL_NUM[0]} -d locisigma=${MODEL_NUM[1]} -d rwide=${MODEL_NUM[2]} -d modelType="${MODEL_NUM[3]}" $TESTDIR/slim/calcLD.slim
 fi
 
 DURATION=$SECONDS
