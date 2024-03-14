bd65194bc655ee60ac4a97691a378e466fb9bb4c
nobrien97 <nicholasobrien97@gmail.com>
2024-03-14 15:02:56 +1000

    Finish summary LD script

--- tests/standingVar/calcLD/LDsummarise/R/sumLD.R
+++ tests/standingVar/calcLD/LDsummarise/R/sumLD.R
@@ -1,9 +1,5 @@
 # Summarise LD information
-
 library(dplyr)
-library(tibble)
-library(tidyr)
-library(readr)
 library(data.table)
 
 # Get command line arguments
@@ -13,33 +9,62 @@ model <- as.numeric(args[1])
 
 GDATA_PATH <- "/g/data/ht96/nb9894/standingVar/calcLD/"
 
-FILE_NAME <- paste0("/scratch/ht96/nb9894/standingVar/calcLD/sumLD", model, ".csv")
+FILE_NAME <- paste0("/scratch/ht96/nb9894/standingVar/calcLD/LDsummarise/sumLD", model, ".csv")
 
 # TODO: check model off by one error
-# ld_pos <- scan(paste0("slim_ld_pos.csv"), skip = model, sep = ",")
+ld_val <- unlist(fread(paste0(GDATA_PATH, "slim_ld_val.csv"), 
+               skip = model, nrows = 1, sep = ","))
 
-
-ld_val <- scan(paste0(GDATA_PATH, "slim_ld_val.csv"), skip = model, sep = ",")
 model_info <- ld_val[1:3]
 ld_val <- ld_val[-(1:3)]
 
-# Maximum number of non-zero element
-max_elements <- 1024*1024
+ld_pos <- unlist(fread(paste0(GDATA_PATH, "slim_ld_pos.csv"), 
+                skip = model, nrows = 1, sep = ","))[-(1:3)]
+
+# Expand to matrix form
+n <- 1024
+max_elements <- n*n
+
+decompressLD <- function(pos, val, n = 1024L) {
+  # Converts a compressed haplotype matrix to a sparse form
+  result <- matrix(integer(n*n), nrow = n, ncol = n)
+  result[pos+1] <- val
+  return(result)
+}
+
+LDmat <- decompressLD(ld_pos, ld_val, n)
+
+# Just get the upper triangle w/o diagonal or zero elements
+LDmat <- LDmat[upper.tri(LDmat) & abs(LDmat) > 0]
+
+# Maximum number of non-zero elements
 
 # Calculate means (don't need pos?)
 # means/sd with and without the zeroes added (just change denominator to include zeroes)
-result <- tibble() %>%
-    mutate(
+result <- tibble(
         gen = model_info[1],
         seed = model_info[2],
         modelindex = model_info[3],
-        meanD = mean(ld_val),
-        sdD = sd(ld_val),
-        nD = length(ld_val),
-        propNonZeroD = length(ld_val)/max_elements,
-        meanDZeros = sum(ld_val) / (max_elements),
-        sdDZeros = sqrt( ( sum((ld_val - meanDZeros)^2) ) / max_elements )
+        meanD = mean(LDmat),
+        sdD = sd(LDmat),
+        meanDZeros = sum(LDmat) / (max_elements),
+        sdDZeros = sqrt( ( sum((LDmat - meanDZeros)^2) ) / max_elements ),
+        nD = length(LDmat),
+        nDP = length(LDmat[LDmat > 0]),
+        nDN = length(LDmat[LDmat < 0]),
+        nDHalf = length(LDmat[abs(LDmat) > 0.5])
     )
+
+# Add counts of 10% groups for a histogram with 21 bins
+labels <- paste0("n", 1:21)
+bins <- seq(-1, 1.1, by = 0.1)
+
+LDbins <- cut(LDmat, breaks = bins, right = F)
+bin_labels <- levels(LDbins)
+
+for (i in seq_along(bin_labels)) {
+    result[,labels[i]] <- length(LDbins[LDbins == bin_labels[i]])
+}
 
 # Write output
 write.table(result, FILE_NAME, row.names = F, col.names = F)
