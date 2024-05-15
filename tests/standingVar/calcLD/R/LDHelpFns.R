708939f4afa4acacdbad3dbdf2b2a4fdb4b24409
nobrien97 <nicholasobrien97@gmail.com>
2024-05-15 18:02:39 +1000

    calculate LD, vectorise relabelling function, fix bugs

--- tests/standingVar/calcLD/R/LDHelpFns.R
+++ tests/standingVar/calcLD/R/LDHelpFns.R
@@ -3,14 +3,20 @@
 # Relabel genotypes according to fitness
 
 RelabelGenotypeFrequencies <- function(d_rankings, l_parentalFreqs) {
+  
+  ranks <- d_rankings %>% select(starts_with("wpar"))
   # Get the labels
-  genotype_names <- paste0("p", substr(colnames(d_rankings)[6:9], 5, 6))
-  ab_id <- genotype_names[apply(d_rankings[, 6:9], 1, which.min)]
-  AB_id <- genotype_names[apply(d_rankings[, 6:9], 1, which.max)]
+  genotype_names <- paste0("p", substr(colnames(ranks), 5, 6))
+  
+  # order rankings by fitness
+  order_indices <- t(apply(ranks, 1, order))
+
+  ab_id <- genotype_names[order_indices[,1]]
+  AB_id <- genotype_names[order_indices[,4]]
   
   # Intermediates can be randomly assigned
-  Ab_id <- genotype_names[apply(d_rankings[, 6:9], 1, function(x) {(1:4)[-c(which.max(x), which.min(x))][1]})]
-  aB_id <- genotype_names[apply(d_rankings[, 6:9], 1, function(x) {(1:4)[-c(which.max(x), which.min(x))][2]})]
+  Ab_id <- genotype_names[order_indices[,2]]
+  aB_id <- genotype_names[order_indices[,3]]
   
   # Create output
   out <- list(pAB = numeric(length(AB_id)),
@@ -19,12 +25,26 @@ RelabelGenotypeFrequencies <- function(d_rankings, l_parentalFreqs) {
               pab = numeric(length(AB_id)))
   
   # Fill output
-  for (i in seq_along(AB_id[1:10000])) {
-    out[["pAB"]][[i]] <- l_parentalFreqs[[AB_id[i]]][[i]]
-    out[["pAb"]][[i]] <- l_parentalFreqs[[Ab_id[i]]][[i]]
-    out[["paB"]][[i]] <- l_parentalFreqs[[aB_id[i]]][[i]]
-    out[["pab"]][[i]] <- l_parentalFreqs[[ab_id[i]]][[i]]
-  }
+  # Convert l_parentalFreqs to a matrix for vectorized indexing
+  n <- length(AB_id)
+  parentalMat <- t(do.call(cbind, l_parentalFreqs))
   
+  # Vectorized filling of the output lists
+  out$pAB <- mapply(function(geno, i) parentalMat[geno, i], AB_id, seq_len(n))
+  out$pAb <- mapply(function(geno, i) parentalMat[geno, i], Ab_id, seq_len(n))
+  out$paB <- mapply(function(geno, i) parentalMat[geno, i], aB_id, seq_len(n))
+  out$pab <- mapply(function(geno, i) parentalMat[geno, i], ab_id, seq_len(n))  
   return(out)
+}
+
+CalcLD <- function(l_freqs, metric = "D") {
+  names(l_freqs$pAB) <- NULL
+  names(l_freqs$pAb) <- NULL
+  names(l_freqs$paB) <- NULL
+  names(l_freqs$pab) <- NULL
+  
+  switch (metric,
+    "D" = return( ( l_freqs$pAB * l_freqs$pab ) - ( l_freqs$pAb * l_freqs$paB ))
+  )
+  
 }
