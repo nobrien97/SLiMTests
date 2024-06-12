c9185500bec19d4d5c9c9f6f340aea86b685ce44
nobrien97 <nicholasobrien97@gmail.com>
2024-06-12 15:46:30 +1000

    handle missing data in covariance matrices

--- tests/standingVar/getH2/R/getCovarianceMatrices.cpp
+++ tests/standingVar/getH2/R/getCovarianceMatrices.cpp
@@ -13,11 +13,21 @@ List extractCovarianceMatrices(DataFrame df) {
   // Iterate over column names to extract unique variable names
   for (int i = 0; i < col_names.size(); ++i) {
     std::string col_name = as<std::string>(col_names[i]);
-    size_t pos = col_name.find("_");
-    std::string var1 = col_name.substr(0, pos);
-    std::string var2 = col_name.substr(pos + 1);
-    variables_set.insert(var1);
-    variables_set.insert(var2);
+    if (col_name.substr(0,2) == "VA") {
+      // Variances
+      std::string var = col_name.substr(3);
+      variables_set.insert(var);
+    }
+    if (col_name.substr(0,3) == "CVA") {
+      // Covariances
+      size_t first_underscore = col_name.find("_");
+      std::string vars = col_name.substr(first_underscore+1);
+      size_t second_underscore = vars.find("_");
+      std::string var1 = vars.substr(0, second_underscore);
+      std::string var2 = vars.substr(second_underscore+1);
+      variables_set.insert(var1);
+      variables_set.insert(var2);
+    }
   }
   
   // Convert the set of variables to a vector
@@ -33,6 +43,9 @@ List extractCovarianceMatrices(DataFrame df) {
   // Initialize a list to store the covariance matrices
   List covariance_matrices(num_rows);
   
+  // vector to keep track of which columns have NA values
+  std::vector<bool> has_na(n, false);
+  
   // Fill the list with covariance matrices for each row
   for (int row = 0; row < num_rows; ++row) {
     // Initialize an empty matrix to store covariances
@@ -41,36 +54,58 @@ List extractCovarianceMatrices(DataFrame df) {
     // Fill the covariance matrix
     for (int i = 0; i < col_names.size(); ++i) {
       std::string col_name = Rcpp::as<std::string>(col_names[i]);
-      size_t pos = col_name.find("_");
-      std::string var1 = col_name.substr(0, pos);
-      std::string var2 = col_name.substr(pos + 1);
-      int idx1 = var_index[var1];
-      int idx2 = var_index[var2];
-      double value = Rcpp::as<NumericVector>(df[i])[row];
-      covariance_matrix(idx1, idx2) = value;
-      covariance_matrix(idx2, idx1) = value; // Because covariance matrices are symmetric
-    }
-    
-    // Fill diagonal with variances (covariance with itself)
-    for (int i = 0; i < n; ++i) {
-      std::string var = variables[i];
-      std::string diag_col = var + "_" + var;
-      if (df.containsElementNamed(diag_col.c_str())) {
-        covariance_matrix(i, i) = Rcpp::as<NumericVector>(df[diag_col])[row];
+      if (col_name.substr(0, 2) == "VA") {
+        // Diagonal elements
+        std::string var = col_name.substr(3);
+        int idx = var_index[var];
+        double value = Rcpp::as<NumericVector>(df[i])[row];
+        covariance_matrix(idx, idx) = value;
+        
+        // If the variance for this variable is NA, mark it for removal later
+        if (NumericVector::is_na(value)) {
+          has_na[idx] = true;
+        }
+      } else if (col_name.substr(0, 3) == "CVA") {
+        // Off-diagonal elements
+        size_t first_underscore = col_name.find("_", 4);
+        std::string var1 = col_name.substr(4, first_underscore - 4);
+        std::string var2 = col_name.substr(first_underscore + 1);
+        int idx1 = var_index[var1];
+        int idx2 = var_index[var2];
+        double value = Rcpp::as<NumericVector>(df[i])[row];
+
+        covariance_matrix(idx1, idx2) = value;
+        covariance_matrix(idx2, idx1) = value; // Because covariance matrices are symmetric
       }
     }
+    
+    // Remove NA
+    std::vector<int> keep_indices;
+    for (int i = 0; i < n; ++i) {
+      if (!has_na[i]) {
+        keep_indices.push_back(i);
+      }
+    }
+    
+    // Create a smaller matrix discarding NA values
+    int m = keep_indices.size();
+    NumericMatrix out_matrix(m, m);
+    CharacterVector out_var_names(m);
+    
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < m; ++j) {
+        out_matrix(i, j) = covariance_matrix(keep_indices[i], keep_indices[j]);
+      }
+      out_var_names[i] = variables[keep_indices[i]];
+    }
+    
     // Store the covariance matrix in the list
-    covariance_matrices[row] = covariance_matrix;
+    covariance_matrices[row] = out_matrix;
+    colnames(covariance_matrices[row]) = out_var_names;
+    rownames(covariance_matrices[row]) = colnames(covariance_matrices[row]);
   }
   
   return covariance_matrices;
 }
 
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
 
-/*** R
-timesTwo(42)
-*/
