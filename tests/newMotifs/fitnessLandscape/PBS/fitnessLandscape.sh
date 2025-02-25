3e72a790366c8ffd33afca332a75ef61cd02be88
nobrien97 <nicholasobrien97@gmail.com>
2025-02-25 17:43:41 +1000

    Add job for estimating fitness landscape

---
+++ tests/newMotifs/fitnessLandscape/PBS/fitnessLandscape.sh
@@ -0,0 +1,94 @@
+#!/bin/bash -l
+#PBS -P ht96
+#PBS -q normalsr
+#PBS -l walltime=48:00:00
+#PBS -l ncpus=208
+#PBS -l mem=1000GB
+#PBS -l jobfs=800GB
+#PBS -l storage=scratch/ht96+gdata/ht96
+  
+  
+ECHO=/bin/echo
+JOBNAME=newMotifs/fitnessLandscape
+FULLJOBNAME=$JOBNAME
+#
+# These variables are assumed to be set:
+#   NJOBS is the total number of jobs in a sequence of jobs (defaults to 1)
+#   NJOB is the number of the current job in the sequence (defaults to 0)
+#   For this job, NJOBS should = 0
+  
+if [ X$NJOBS == X ]; then
+    $ECHO "NJOBS (total number of jobs in sequence) is not set - defaulting to 1"
+    export NJOBS=1
+fi
+  
+if [ X$NJOB == X ]; then
+    $ECHO "NJOB (current job number in sequence) is not set - defaulting to 0"
+    export NJOB=0
+    # Since this is the first iteration, create our folders
+    $ECHO "Creating outputs folders..."
+    cd $PBS_O_WORKDIR
+
+    # Make output folder
+    mkdir -p /scratch/ht96/nb9894/$FULLJOBNAME
+    mkdir -p /g/data/ht96/nb9894/$FULLJOBNAME
+fi
+
+#
+# Quick termination of job sequence - look for a specific file 
+#
+if [ -f STOP_SEQUENCE ] ; then
+    $ECHO  "Terminating sequence at job number $NJOB"
+    exit 0
+fi
+
+$ECHO "Starting job $NJOB of $NJOBS"
+
+# Pre-job file manipulation goes here ...
+# 
+# INSERT CODE
+cd $PBS_O_WORKDIR
+SAVEDIR=/g/data/ht96/nb9894/$FULLJOBNAME
+
+
+# ========================================================================
+# .... USER INSERTION OF EXECUTABLE LINE HERE 
+# ========================================================================
+module load R/4.0.0
+module load intel-compiler/2021.10.0
+
+$ECHO "Calculating landscape"
+
+Rscript ../R/fitnessOptima.R
+
+# Copy output
+# Combine output into a single file
+cd /scratch/ht96/nb9894/$FULLJOBNAME/
+mv ./fitnessOptima.RDS $SAVEDIR/fitnessOptima.RDS
+
+# 
+# Check the exit status
+#
+errstat=$?
+if [ $errstat -ne 0 ]; then
+    # A brief nap so PBS kills us in normal termination
+    # If execution line above exceeded some limit we want PBS
+    # to kill us hard
+    sleep 5 
+    $ECHO "Job number $NJOB returned an error status $errstat - stopping job sequence."
+    exit $errstat
+fi
+
+#   
+# Are we in an incomplete job sequence - more jobs to run ?
+#   
+if [ $NJOB -lt $NJOBS ]; then
+# Now increment counter and submit the next job
+# 
+    NJOB=$(($NJOB+1))
+    $ECHO "Submitting job number $NJOB in sequence of $NJOBS jobs"
+    cd $PBS_O_WORKDIR
+    qsub -v NJOBS=$NJOBS,NJOB=$NJOB ./$JOBNAME.sh
+else
+    $ECHO "Finished last job in sequence of $NJOBS jobs"
+fi
