fbb9986a7fc41c879e63853ae38f86b6bc10a4d3
nobrien97 <nicholasobrien97@gmail.com>
2025-04-05 12:43:51 +1000

    Update extraReplicates/getH2 run count

--- tests/newMotifs/extraReplicates/h2/getH2/R/combos.R
+++ tests/newMotifs/extraReplicates/h2/getH2/R/combos.R
@@ -2,7 +2,7 @@
 filename <- "./getH2SR.sh"
 
 # Run numbers to extract haplotypes
-run <- 0:1439 # TODO: Update after the extraReplicates has run
+run <- 0:2879
 
 # Chunk numbers to collect output: from 1 to 2, which is 1440 files per chunk
 
@@ -11,7 +11,7 @@ chunk <- 1:2
 cmd_df <- data.frame(
     filename = filename,
     run = run,
-    chunk = rep(chunk, each = 720)
+    chunk = rep(chunk, each = 1440)
 )
 
 write.table(cmd_df, "/mnt/c/GitHub/SLiMTests/tests/newMotifs/extraReplicates/h2/getH2/PBS/cmds.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
