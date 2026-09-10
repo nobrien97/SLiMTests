a2ac97b0ac11a0ce3672327166199295e81d63a5
nobrien97 <nicholasobrien97@gmail.com>
2026-09-10 11:33:19 +1000

    plot full scale PCA landscape on HPC with all data

--- tests/newMotifs/paper1/analysis/R/ruggedness_landscape.R
+++ tests/newMotifs/paper1/analysis/R/ruggedness_landscape.R
@@ -298,7 +298,7 @@ d_pca_codings <- d_pca_codings %>%
                                               "Randomised")))
 
 saveRDS(d_pca_codings, "/mnt/i/SLiMTests/tests/newMotifs/paper1/ruggedness/d_pca_codings.RDS")
-d_pca_codings <- readRDS("/mnt/i/SLiMTests/tests/newMotifs/paper1/ruggedness/d_pca_codings.RDS")
+d_pca_codings <- readRDS("/mnt/d/SLiMTests/tests/newMotifs/paper1/ruggedness/d_pca_codings.RDS")
 
 plt_pca <- ggplot(d_pca_codings,
                       aes(x = PC1, y = PC2, colour = fitness)) +
@@ -328,16 +328,17 @@ d_pca_grid <- d_pca_codings %>% select(model, dataset, PC1, PC2, fitness) %>%
   summarise(z = mean(fitness))
 
 
-contour_pal <- c("#220022", paletteer_d("ggprism::viridis", 6)[-1])
+contour_pal <- c("#220022", paletteer_d("ggprism::viridis", 6)[-c(1)])
 
 plt_pca_tile <- ggplot(d_pca_grid %>% drop_na(),
-                     aes(x = x, y = y, fill = z, z = z, group = z)) +
+                     aes(x = x, y = y, fill = tanh(z), z = z, group = z)) +
   facet_manual(model ~ ., design = design) +
   geom_tile() +
-  scale_fill_gradientn(colours = contour_pal,
-                    breaks = c(0, seq(0.1, 1.0, by = 0.3)),
-                    #labels = seq(0, 1, by = 0.25),
-                    limits = c(0, 1)) + 
+  # scale_fill_gradientn(colours = contour_pal,
+  #                   breaks = c(0, seq(0.1, 1.0, by = 0.3)),
+  #                   #labels = seq(0, 1, by = 0.25),
+  #                   limits = c(0, 1)) + 
+  scale_fill_gradientn(colours = contour_pal) +
   #scale_fill_viridis_c() +
   labs(x = "PC1", y = "PC2", 
        fill = "Fitness") +
@@ -346,7 +347,11 @@ plt_pca_tile <- ggplot(d_pca_grid %>% drop_na(),
         legend.position = "bottom",
         legend.key.width = unit(3.5, 'line'))
 plt_pca_tile
+ggsave("plt_pca_tile.png", plt_pca_tile, device = png, 
+       width = 11, height = 8, dpi = 600, bg = "white")
 
+
+# Interpolated landscape
 # Fast interpolation via k nearest neighbours
 get.nn <- function(data, labels, query) {
   nns <- get.knnx(data, query, k=1)
@@ -374,16 +379,10 @@ d_pca_grid <- purrr::map(seq_along(d_pca_grid), function(i) {
 d_pca_grid <- data.table::rbindlist(d_pca_grid)  
 
 
-plt_pca_contour <- ggplot(d_pca_grid %>% drop_na(),
-                          aes(x = x, y = y, fill = z, z = z, group = z)) +
+plt_pca_contour <- ggplot(d_pca_grid,
+                          aes(x = x, y = y, z = z)) +
   facet_manual(model ~ ., design = design) +
-  #geom_raster() +
-  geom_contour(aes(colour = z)) +
-  scale_fill_gradientn(colours = contour_pal,
-                       breaks = c(0, seq(0.1, 1.0, by = 0.3)),
-                       #labels = seq(0, 1, by = 0.25),
-                       limits = c(0, 1)) + 
-  #scale_fill_viridis_c() +
+  geom_contour_filled() +
   labs(x = "PC1", y = "PC2", 
        fill = "Fitness") +
   theme_bw() +
@@ -392,3 +391,84 @@ plt_pca_contour <- ggplot(d_pca_grid %>% drop_na(),
         legend.key.width = unit(3.5, 'line'))
 plt_pca_contour
 
+
+##########################################
+### HPC
+# Try PCA on whole dataset
+PCPerModel <- function(data, z, columns) {
+  data <- data %>% select(all_of(columns))
+  pc <- prcomp(data, scale = T)
+  result <- data.frame(PC1 = pc$x[,1],
+                       PC2 = pc$x[,2],
+                       z = z)
+  return(result)
+}
+
+pc_ruggedness <- d_ruggedness %>%
+  filter(fitness >= 0.0) %>%
+  rename(h = Hilln,
+         gX = XMult,
+         zZ = base) %>%
+  mutate(model = factor(model, levels = model_names_noquote)) %>%
+  group_by(model) %>%
+  group_map(~ PCPerModel(.x, .x$fitness, unlist(molComp_names[as.character(.x$model[1])])))
+
+pc_ruggedness <- purrr::map(seq_along(model_names_noquote), function(i) {
+  x <- d_ruggedness %>%
+    filter(fitness >= 0.0) %>%
+    rename(h = Hilln,
+           gX = XMult,
+           zZ = base) %>%
+    mutate(model = factor(model, levels = model_names_noquote)) %>%
+    filter(model == model_names_noquote[i])
+  
+  z <- x$fitness
+  column_names <- (molComp_names[model_names_noquote[i]])[[1]]
+  result <- PCPerModel(x, z, column_names)
+  result$model <- model_names_noquote[i]
+  return(result)
+  
+}, .progress = T)
+
+d_pc_ruggedness <- data.table::rbindlist(pc_ruggedness, fill = T)
+
+downsample <- 1/15
+d_pc_ruggedness_ds <- d_pc_ruggedness %>% select(model, PC1, PC2, z) %>%
+  group_by(model,
+           x = downsample * round(PC1 / downsample),
+           y = downsample * round(PC2 / downsample)) %>%
+  summarise(z = mean(z))
+
+nrow(d_pc_ruggedness_ds)
+
+# Plot
+contour_pal <- c("#220022", paletteer_d("ggprism::viridis", 6)[-c(1)])
+design <- "
+AABBCC
+#DDEE#
+"
+
+plt_pca_tile <- ggplot(d_pc_ruggedness_ds %>% drop_na() %>%
+                         mutate(model = factor(model, 
+                                               levels = model_names_noquote)),
+                       aes(x = x, y = y, fill = z, z = z, group = z)) +
+  facet_manual(model ~ ., design = design) +
+  geom_raster() +
+  scale_fill_gradientn(colours = contour_pal,
+                    breaks = c(0, seq(0.1, 1.0, by = 0.3)),
+                    #labels = seq(0, 1, by = 0.25),
+                    limits = c(0, 1)) +
+  #scale_fill_gradientn(colours = contour_pal) +
+  labs(x = "PC1", y = "PC2", 
+       fill = "Fitness") +
+  theme_bw() +
+  theme(text = element_text(size=12), 
+        legend.position = "bottom",
+        legend.key.width = unit(3.5, 'line'))
+plt_pca_tile
+ggsave("plt_pca_tile_full.png", plt_pca_tile, device = png, 
+       width = 11, height = 8, dpi = 600, bg = "white")
+
+
+
+##########################################
